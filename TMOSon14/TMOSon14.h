2bc3dc4a09d2bd4b6df6377182fa427e7f840679
xsedla0k <Sedlar17@seznam.cz>
2017-12-17 22:09:34 +0100

    TODO: detailMaximization, detailControl, etc.
    editing for cleaner code
    adding comments

---
+++ TMOSon14/TMOSon14.h
@@ -0,0 +1,28 @@
+#include "TMO.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <math.h>
+#include <vector>
+
+#ifdef EPS
+#undef EPS
+#define EPS EPS2
+#endif
+#include "opencv2/opencv.hpp"
+#include "L0minimization.h"
+#include "baseAndDetailDecomposition.h"
+
+#include <Eigen/Sparse>
+#undef EPS
+					                                                   
+class TMOSon14: public TMO  
+{
+public:
+	TMOSon14();
+	virtual ~TMOSon14();
+	virtual int Transform();	
+
+protected:
+	TMODouble mu;
+};
