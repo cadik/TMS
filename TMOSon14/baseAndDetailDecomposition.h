783fa9023d9d6d0a28d4c222c4b8d8e80d2dfadb
xsedla0k <Sedlar17@seznam.cz>
2017-12-18 22:01:41 +0100

    fixed indentation

--- TMOSon14/baseAndDetailDecomposition.h
+++ TMOSon14/baseAndDetailDecomposition.h
@@ -1,5 +1,5 @@
 /**
- * Zde uz jsou zahrnuty vsechny fce z originalniho l0minimalization
+ * My functions
  */
 #include <fstream>
 #include <opencv2/opencv.hpp>
@@ -8,43 +8,19 @@
 
 extern cv::Mat minimizeL0Gradient1(const cv::Mat &src);
 
-extern cv::Mat getAdaptiveLambdaMatrix(const cv::Mat &gradient, 
-									   int rows, 
-									   int cols);
+extern cv::Mat getAdaptiveLambdaMatrix(const cv::Mat &gradient, int rows, int cols);
 
-extern void optimizeWithAdaptiveLambdaMatrix(cv::Mat &S, 
-										     const cv::Mat &I, 
-										     cv::Mat &H, 
-										     cv::Mat &V, 
-										     cv::Mat &grad_x,
-										     cv::Mat &grad_y,
-										     cv::Mat &lambdaMatrix,
-										     float &beta);
+extern void optimizeWithAdaptiveLambdaMatrix(cv::Mat &S, const cv::Mat &I, cv::Mat &H, cv::Mat &V, cv::Mat &grad_x,cv::Mat &grad_y,cv::Mat &lambdaMatrix,float &beta);
 
-extern cv::Mat minimizeL0GradientSecondFaze(const cv::Mat &src, 
-											cv::Mat lambdaMatrix1, 
-											int rows, 
-											int cols);
+extern cv::Mat minimizeL0GradientSecondFaze(const cv::Mat &src, cv::Mat lambdaMatrix1, int rows, int cols);
 
 extern cv::Mat getGradientMagnitude(const cv::Mat &src);
 
-extern cv::Mat getDetailLayer(const cv::Mat &orig, 
-							  const cv::Mat &base, 
-							  int rows, 
-							  int cols);
+extern cv::Mat getDetailLayer(const cv::Mat &orig, const cv::Mat &base, int rows, int cols);
 							  
-extern cv::Mat getWeightsFromBaseLayer(const cv::Mat &gradient, 
-									   int rows, 
-									   int cols,
-									   int r);
+extern cv::Mat getWeightsFromBaseLayer(const cv::Mat &gradient, int rows, int cols,int r);
 							  
-extern cv::Mat getDetail(const cv::Mat &base, 
-						 const cv::Mat &detail,
-					     const cv::Mat &s,
-						 const cv::Mat &t,
-						 float mu, 
-						 int rows, 
-						 int cols);
+extern cv::Mat getDetail(const cv::Mat &base, const cv::Mat &detail, const cv::Mat &s, const cv::Mat &t, float mu, int rows, int cols);
 						 
 extern cv::Mat stochasticOptimizationForGetSigma(cv::Mat base, cv::Mat original, int rows, int cols, int counter);
 
