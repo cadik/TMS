1ecc444459ba6279ed73aaa9db41ba8daf9312bd
xsedla0k <Sedlar17@seznam.cz>
2018-01-03 01:21:33 +0100

    stochastic optimization
    přidány funkce pro minimalizaci funkce, zatím stochasticky + kontrola detailů

--- TMOSon14/baseAndDetailDecomposition.cpp
+++ TMOSon14/baseAndDetailDecomposition.cpp
@@ -84,6 +84,33 @@ cv::Mat getGradientMagnitude(const cv::Mat &src)
 }
 
 /*
+ * get gradient from one layer
+ **/
+ cv::Mat getGradientMagnitudeFromOneLayer(const cv::Mat &src)
+{
+	int rows = src.rows;
+    int cols = src.cols;
+    
+
+    cv::Mat S, grad_x, grad_y, gradient;   
+    S = cv::Mat(rows, cols, CV_32FC1);
+    grad_x = cv::Mat::zeros(rows, cols, CV_32FC1);
+    grad_y = cv::Mat::zeros(rows, cols, CV_32FC1); 
+    gradient = cv::Mat::zeros(rows, cols, CV_32FC1);  
+
+
+	computeGradient(src, grad_x, grad_y);
+	for(int j=0; j<rows; j++){
+		for(int i=0; i<cols; i++){
+			float gx = grad_x.at<float>(j, i);
+			float gy = grad_y.at<float>(j, i);
+			gradient.at<float>(j, i) += sqrt(pow(gx, 2) + pow(gy, 2));
+		}
+	}
+    return gradient;
+}
+
+/*
  * Function for getting adaptive lambda matrix from gradient magnitude 
  **/
 cv::Mat getAdaptiveLambdaMatrix(const cv::Mat &gradient, 
@@ -113,7 +140,7 @@ cv::Mat getAdaptiveLambdaMatrix(const cv::Mat &gradient,
                 tmp = 0;
             }
             /*
-		Final calculation of adaptive lambda for element
+				Final count of adaptiveLambdaMatrix
             */
             adaptiveLambdaMatrix.at<float>(j, i) = 3*(0.1 - epsilon)*tmp + epsilon;
         }
@@ -228,17 +255,14 @@ cv::Mat getWeightsFromBaseLayer(const cv::Mat &gradient, int rows, int cols, int
 	double a = 0.2;
     // initialize
     cv::Mat weights; 
-    weights = cv::Mat::zeros(rows, cols, CV_32FC1);  
+    weights = cv::Mat::zeros(rows, cols, CV_32F);  
     
-    /*
-    	Calculation of tricube function for getting weights
-    */
     for (int j = 0; j < rows; j++) {
 		for (int i = 0; i < cols; i++) {
 			if (abs(gradient.at<float>(j, i)/a) <= 1) {
 				weights.at<float>(j, i) = pow(1 - pow(abs(gradient.at<float>(j, i)/a), 3), 3);
 				
-				if (weights.at<float>(j, i) * r <= 2) {
+				if (weights.at<float>(j, i) * r <= 2.0) {
 					weights.at<float>(j, i) += 2.0/(float)r;
 				}
 			} else {
@@ -248,6 +272,98 @@ cv::Mat getWeightsFromBaseLayer(const cv::Mat &gradient, int rows, int cols, int
 	}
     return weights;
 }
+/*
+ * Function for detail maximilization 
+ **/
+ 
+std::vector<cv::Mat> detailMaximalization(const cv::Mat &base, const cv::Mat &detailSum, const cv::Mat &r1Weight, const cv::Mat &r2Weight,  int rows, int cols, int counter, std::vector<cv::Mat> detail) 
+{
+	srand (time(NULL));
+    std::vector<cv::Mat> S_mats;
+    
+    long double tmpSum = 1e45;
+    
+    int r1 = 200;
+    int r2 = 500;
+    
+    cv::Mat S;
+	cv::Mat T;
+    
+	cv::Mat	tmpS = cv::Mat::zeros(rows, cols, CV_32FC1);
+	cv::Mat	tmpT = cv::Mat::zeros(rows, cols, CV_32FC1);
+    for (int a = 0; a < counter; a++) {
+		/*
+		 * Getting random layer
+		 * */
+		long double sumR1 = 0;
+		long double sumR2 = 0;
+		S = cv::Mat::zeros(rows, cols, CV_32FC1);
+		T = cv::Mat::zeros(rows, cols, CV_32FC1);
+		long double sum = 0;
+		for(int j = 0; j < rows; j++){
+			for(int i = 0; i < cols; i++){
+				/*
+				 * Constraint condition 
+				 **/
+				while(true) {
+					double aS = rand() % 10000;
+					double bS = rand() % 10000;
+					double cS = rand() % 1000;
+					
+					S.at<float>(j, i) = ((double)aS - (double)bS) / (double)cS;
+					
+					double aT = rand() % 10000;
+					double bT = rand() % 10000;
+					double cT = rand() % 1000;
+					
+					T.at<float>(j, i) = ((double)aT - (double)bT) / (double)cT;
+					
+					bool cond1 = false;
+					bool cond2 = false;
+					if ((base.at<float>(j,i) + T.at<float>(j, i) + S.at<float>(j, i) * (detail[0]).at<float>(j, i)) >= 0 && (base.at<float>(j,i) + T.at<float>(j, i) + S.at<float>(j, i) * (detail[0]).at<float>(j, i)) <= 1) {
+						cond1 = true;
+					}
+					
+					if ((cond1)&&(base.at<float>(j,i) + T.at<float>(j, i) + S.at<float>(j, i) * (detail[1]).at<float>(j, i)) >= 0 && (base.at<float>(j,i) + T.at<float>(j, i) + S.at<float>(j, i) * (detail[1]).at<float>(j, i)) <= 1) {
+						cond2 = true;
+					}
+					
+					if ((cond1)&&(cond2)&&(base.at<float>(j,i) + T.at<float>(j, i) + S.at<float>(j, i) * (detail[2]).at<float>(j, i)) >= 0 && (base.at<float>(j,i) + T.at<float>(j, i) + S.at<float>(j, i) * (detail[2]).at<float>(j, i)) <= 1) {
+						break;
+					}
+					
+					// break;
+				}				
+			}
+		}
+		/*
+		 * getting gradients of T and S
+		 * */
+		cv::Mat gradientT = getGradientMagnitudeFromOneLayer(T);
+		cv::Mat gradientS = getGradientMagnitudeFromOneLayer(S);
+		
+		for(int jj = 0; jj < rows; jj++){
+			for(int ii = 0; ii < cols; ii++){
+				sum -= pow(S.at<float>(jj, ii) * detailSum.at<float>(jj, ii), 2);
+				sumR1 += r1Weight.at<float>(jj, ii) * pow(gradientS.at<float>(jj, ii), 2);
+				sumR2 += r2Weight.at<float>(jj, ii) * pow(gradientT.at<float>(jj, ii), 2);
+			}
+		}
+		
+		sumR1 *= r1;
+		sumR2 *= r2;
+		
+		if ((sum + sumR1 + sumR2) < tmpSum) {
+			tmpSum = sum + sumR1 + sumR2;
+			tmpS = S;
+			tmpT = T;
+		}
+	}
+    S_mats.push_back(tmpS.clone());
+    S_mats.push_back(tmpT.clone());
+    
+    return S_mats;
+}
 
 /*
  * Function for detail control
@@ -256,8 +372,9 @@ cv::Mat getDetailControl(const cv::Mat &base, const cv::Mat &detail,const cv::Ma
 {
 		cv::Mat detailLayer;
 		detailLayer = cv::Mat::zeros(rows, cols, CV_32F);
-
-	for(int j=0; j<rows; j++){
+		(s).convertTo(s, CV_32F);
+		(t).convertTo(t, CV_32F);
+		for(int j=0; j<rows; j++){
             for(int i=0; i<cols; i++){
                 detailLayer.at<float>(j, i) = (mu * s.at<float>(j, i) + (1 - mu)) * detail.at<float>(j, i) + base.at<float>(j, i) + mu * t.at<float>(j, i);
             }
@@ -286,7 +403,7 @@ cv::Mat stochasticOptimizationForGetSigma(cv::Mat base, cv::Mat original, int ro
 			cv::Mat blurredImage;		
 			blurredImage = cv::Mat::zeros(rows, cols, CV_32F);
 			/*
-			 * At first getting random elements with discrete values
+			 * Getting random layer with discrete values
 			 **/
 			for(int j=0; j<rows; j++){
 				for(int i=0; i<cols; i++){
@@ -295,27 +412,19 @@ cv::Mat stochasticOptimizationForGetSigma(cv::Mat base, cv::Mat original, int ro
 			}
 			
 			/*
-			 * Getting blurred version with adaptive sigma
+			 * Getting blurred version of image
 			 **/
 			cv::Mat rSmoothed = myOwn2DFilter(base, randomLayer, rows, cols);
 			
 			/*
-			 * Getting parcial derivation
+			 * Getting derivations
 			 **/
-			std::vector<cv::Mat> array_to_merge;
-
-			array_to_merge.push_back(randomLayer);
-			array_to_merge.push_back(randomLayer);
-			array_to_merge.push_back(randomLayer);
-
-			cv::Mat randomLayerForMagnitude;
 			
-			cv::merge(array_to_merge, randomLayerForMagnitude);
+			cv::Mat randomLayerMagnitude = getGradientMagnitudeFromOneLayer(randomLayer);
 			
-			cv::Mat randomLayerMagnitude = getGradientMagnitude(randomLayerForMagnitude);
 			/*
-			 * Minimization
-			 **/
+			 * Minimalization
+			 * */
 			long double sum = 0.0;
 			 
 			for(int j=0; j<rows; j++){
@@ -323,9 +432,7 @@ cv::Mat stochasticOptimizationForGetSigma(cv::Mat base, cv::Mat original, int ro
 					sum += pow(rSmoothed.at<float>(j, i) - original.at<float>(j, i), 2) + y * pow(randomLayerMagnitude.at<float>(j, i), 2);
 				}
 			}
-			/*
-			 * Replace sum and tmpLayer with better result of minimization
-			 **/
+			
 			if (sum < tmpSum) {
 				tmpSum = sum;
 				tmpLayer = randomLayer;
@@ -366,15 +473,22 @@ cv::Mat myOwn2DFilter(cv::Mat image, cv::Mat sigmaMap, int rows, int cols)
 	{
 		for(int i =0; i < cols; ++i)
 		{
-			// Getting gaussian kernel
+			/*
+			 * getting gaussian kernel
+			 * */
 			long double sum = 0;
 			cv::Mat gaussFilter;
-			gaussFilter = cv::Mat::zeros(sigmaMap.at<float>(j, i) * 2 + 1, sigmaMap.at<float>(j, i) * 2 + 1, CV_32F);
-			gaussFilter = cv::getGaussianKernel(
-				sigmaMap.at<float>(j, i) * 2 + 1,
-				sigmaMap.at<float>(j, i) / 3.0,
-				CV_32F
-			);
+			if (sigmaMap.at<float>(j, i) != 0) {
+				gaussFilter = cv::Mat::zeros(sigmaMap.at<float>(j, i) * 2 + 1, sigmaMap.at<float>(j, i) * 2 + 1, CV_32F);
+				gaussFilter = cv::getGaussianKernel(
+					sigmaMap.at<float>(j, i) * 2 + 1,
+					sigmaMap.at<float>(j, i) / 3.0,
+					CV_32F
+				);
+			} else {
+				gaussFilter = cv::Mat::ones(1, 1, CV_32F);
+			}
+			
 			
 			cv::Mat gauss2DFilter = gaussFilter * gaussFilter.t();
 			
--- TMOSon14/baseAndDetailDecomposition.h
+++ TMOSon14/baseAndDetailDecomposition.h
@@ -1,5 +1,5 @@
 /**
- * My functions
+ * Zde uz jsou zahrnuty vsechny fce z originalniho l0minimalization
  */
 #include <fstream>
 #include <opencv2/opencv.hpp>
@@ -8,19 +8,47 @@
 
 extern cv::Mat minimizeL0Gradient1(const cv::Mat &src);
 
-extern cv::Mat getAdaptiveLambdaMatrix(const cv::Mat &gradient, int rows, int cols);
+extern cv::Mat getAdaptiveLambdaMatrix(const cv::Mat &gradient, 
+									   int rows, 
+									   int cols);
 
-extern void optimizeWithAdaptiveLambdaMatrix(cv::Mat &S, const cv::Mat &I, cv::Mat &H, cv::Mat &V, cv::Mat &grad_x,cv::Mat &grad_y,cv::Mat &lambdaMatrix,float &beta);
+extern void optimizeWithAdaptiveLambdaMatrix(cv::Mat &S, 
+										     const cv::Mat &I, 
+										     cv::Mat &H, 
+										     cv::Mat &V, 
+										     cv::Mat &grad_x,
+										     cv::Mat &grad_y,
+										     cv::Mat &lambdaMatrix,
+										     float &beta);
 
-extern cv::Mat minimizeL0GradientSecondFaze(const cv::Mat &src, cv::Mat lambdaMatrix1, int rows, int cols);
+extern cv::Mat minimizeL0GradientSecondFaze(const cv::Mat &src, 
+											cv::Mat lambdaMatrix1, 
+											int rows, 
+											int cols);
 
 extern cv::Mat getGradientMagnitude(const cv::Mat &src);
 
-extern cv::Mat getDetailLayer(const cv::Mat &orig, const cv::Mat &base, int rows, int cols);
+extern cv::Mat getGradientMagnitudeFromOneLayer(const cv::Mat &src);
+
+extern cv::Mat getDetailLayer(const cv::Mat &orig, 
+							  const cv::Mat &base, 
+							  int rows, 
+							  int cols);
 							  
-extern cv::Mat getWeightsFromBaseLayer(const cv::Mat &gradient, int rows, int cols,int r);
+extern cv::Mat getWeightsFromBaseLayer(const cv::Mat &gradient, 
+									   int rows, 
+									   int cols,
+									   int r);
+									   
+extern std::vector<cv::Mat> detailMaximalization(const cv::Mat &base, const cv::Mat &detailSum, const cv::Mat &r1Weight, const cv::Mat &r2Weight, int rows, int cols, int counter, std::vector<cv::Mat> detail); 
 							  
-extern cv::Mat getDetail(const cv::Mat &base, const cv::Mat &detail, const cv::Mat &s, const cv::Mat &t, float mu, int rows, int cols);
+extern cv::Mat getDetailControl(const cv::Mat &base, 
+						 const cv::Mat &detail,
+					     const cv::Mat &s,
+						 const cv::Mat &t,
+						 float mu, 
+						 int rows, 
+						 int cols);
 						 
 extern cv::Mat stochasticOptimizationForGetSigma(cv::Mat base, cv::Mat original, int rows, int cols, int counter);
 
