2729c122bb83abafd4118a969fbf08f41d6fe2ed
freetomik <freetomik@seznam.cz>
2018-03-21 20:32:40 +0100

    first commit, converting to gray

---
+++ TMOAubry14/TMOAubry14.cpp
@@ -0,0 +1,89 @@
+/* --------------------------------------------------------------------------- *
+ * TMOAubry14.cpp: implementation of the TMOAubry14 class.   *
+ * --------------------------------------------------------------------------- */
+
+#include "TMOAubry14.h"
+
+/* --------------------------------------------------------------------------- *
+ * Constructor serves for describing a technique and input parameters          *
+ * --------------------------------------------------------------------------- */
+TMOAubry14::TMOAubry14()
+{
+	SetName(L"Aubry14");						// TODO - Insert operator name
+	SetDescription(L"Tone mapping using fast local Laplacian filters");	// TODO - Insert description
+
+	dParameter.SetName(L"ParameterName");				// TODO - Insert parameters names
+	dParameter.SetDescription(L"ParameterDescription");	// TODO - Insert parameter descriptions
+	dParameter.SetDefault(1);							// TODO - Add default values
+	dParameter=1.;
+	dParameter.SetRange(0.0,1.0);				// TODO - Add acceptable range if needed
+	// this->Register(dParameter);
+}
+
+TMOAubry14::~TMOAubry14()
+{
+}
+
+/* --------------------------------------------------------------------------- *
+ * This overloaded function is an implementation of your tone mapping operator *
+ * --------------------------------------------------------------------------- */
+int TMOAubry14::Transform()
+{
+	// Source image is stored in local parameter pSrc
+	// Destination image is in pDst
+
+	// Initialy images are in RGB format, but you can
+	// convert it into other format
+	// pSrc->Convert(TMO_Yxy);								// This is format of Y as luminance
+	// pDst->Convert(TMO_Yxy);								// x, y as color information
+
+	double* pSourceData = pSrc->GetData();
+	double* pDestinationData = pDst->GetData();
+
+	int height = pSrc->GetHeight();
+	int width  = pSrc->GetWidth();
+
+	double red, green, blue, gray;
+
+	cv::Mat rgbMat, grayMat;
+	rgbMat = cv::Mat::zeros (height, width, CV_32FC3);
+	grayMat = cv::Mat::zeros (height, width, CV_32FC1);
+
+
+	// convert to grayscale
+	int j = 0;
+	for (j = 0; j < height; j++)
+	{
+		pSrc->ProgressBar(j, height);	// You can provide progress bar
+		for (int i = 0; i < width; i++)
+		{
+			// very SLOW, don't know why
+			// but I need also rgbMat to calculate ratio later
+			rgbMat.at<cv::Vec3f>(j,i)[0] = *pSourceData++;
+			rgbMat.at<cv::Vec3f>(j,i)[1] = *pSourceData++;
+			rgbMat.at<cv::Vec3f>(j,i)[2] = *pSourceData++;
+
+			// cvtColor handles max 32b floats
+			cv::cvtColor(rgbMat, grayMat, CV_RGB2GRAY);
+			// grayMat.at<double>(j,i) = 0.299*red + 0.587*green + 0.114*blue;
+
+			// and store results to the destination image
+			*pDestinationData++ = grayMat.at<float>(j,i);
+			*pDestinationData++ = grayMat.at<float>(j,i);
+			*pDestinationData++ = grayMat.at<float>(j,i);
+		}
+	}
+
+	// calculate ratio for converting to rgb at the end
+	// ...
+
+	// calculate LLF
+	// ...
+
+	// multiply result with ratio
+	// ...
+
+	pSrc->ProgressBar(j, pSrc->GetHeight());
+	pDst->Convert(TMO_RGB);
+	return 0;
+}
---
+++ TMOAubry14/TMOAubry14.h
@@ -0,0 +1,25 @@
+#include "TMO.h"
+
+#include <vector>
+
+#ifdef EPS
+#undef EPS
+#define EPS EPS2
+#endif
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#undef EPS
+
+class TMOAubry14 : public TMO
+{
+public:
+	TMOAubry14();
+	virtual ~TMOAubry14();
+	virtual int Transform();
+
+protected:
+	TMODouble dParameter;
+};
