e7a68fdb43500d58d66b0886d62978c9be788512
Vladis11 <vladis.vlkovic@gmail.com>
2018-04-10 07:59:27 +0200

    Kim09 video done

--- TMOKim09/TMOKim09.h
+++ TMOKim09/TMOKim09.h
@@ -14,7 +14,7 @@
 #ifndef TMORC2GNGM_H
 #define TMORC2GNGM_H
 
-#include "TMO.h"
+#include "TMOv.h"
 #include <iostream>
 #include <math.h>
 
@@ -27,21 +27,25 @@
 // lab to luv conversion rate
 #define LAB_TO_LUV 2.55
 
-class TMOKim09 : public TMO  
+class TMOKim09 : public TMOv  
 {
 private:
 	TMODouble alpha;
 	TMOBool verbose;
+	TMODouble beta;
 protected:	
 	double Gradient(double*, double*);	
 	double FunctionF(double theta);
 	double HkEffectPredictor(double *);
 	void PixelLabToLuv(double*, double*);
 	void PixelLchToLab(double*, double*);
+	
+	
 public:
 	TMOKim09();
 	virtual ~TMOKim09();
 	virtual int Transform();
+	virtual int TransformVideo();
 };
 
 #endif
