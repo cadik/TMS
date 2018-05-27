12aa60c883f49e5453d02fb851a08095e9bd9ad6
rsn4ke <rsn4ke@gmail.com>
2018-05-27 18:48:51 +0200

    TMOEisemann04
    Flash/No-Flash

---
+++ TMOEisemann04/TMOPlugin.h
@@ -0,0 +1,18 @@
+#include "TMO.h"
+
+#ifndef LINUX
+#ifdef TMOPLUGIN_EXPORTS
+#define TMOPLUGIN_API __declspec(dllexport)
+#else
+#define TMOPLUGIN_API __declspec(dllimport)
+#endif
+#pragma warning (disable: 4251) 
+#else
+#define TMOPLUGIN_API
+#endif
+
+#define TMOPLUGIN_API
+
+extern "C" TMOPLUGIN_API int EnumOperators(TMO** operators);
+extern "C" TMOPLUGIN_API int DeleteOperators(TMO** operators);
+extern "C" TMOPLUGIN_API int OperatorCount();
