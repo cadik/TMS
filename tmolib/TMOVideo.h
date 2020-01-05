a06d2184125c2a606dc540f25695c21924cfbda7
Jan Jedlicka <jan.jedlic@gmail.com>
2020-01-05 18:58:52 +0100

    Cmake for qt4 troubleshooting

--- tmolib/TMOVideo.h
+++ tmolib/TMOVideo.h
@@ -9,8 +9,8 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/imgcodecs/imgcodecs.hpp"
-#include "opencv2/videoio/videoio.hpp"
+//#include "opencv2/imgcodecs/imgcodecs.hpp"
+//#include "opencv2/videoio/videoio.hpp"
 
 #undef EPS
 
