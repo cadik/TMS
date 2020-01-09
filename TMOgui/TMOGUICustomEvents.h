846c7e8d3912b64ea86e8006b88f0d0d41bffea8
Jan Jedlicka <jan.jedlic@gmail.com>
2020-01-09 23:24:36 +0100

    QT3Support removal

---
+++ TMOgui/TMOGUICustomEvents.h
@@ -0,0 +1,24 @@
+#ifndef TMOGUICUSTOMEVENTS_H
+#define TMOGUICUSTOMEVENTS_H
+
+#include <QEvent>
+
+
+class TMOGUICustomEvent : QEvent
+{
+public:
+
+    TMOGUICustomEvent(QEvent::Type type, void* event_data) : QEvent(type)
+    {
+        eventdata = event_data;
+    }
+
+    void* data(){
+        return eventdata;
+    }
+
+protected:
+    void* eventdata;
+};
+
+#endif // TMOGUICUSTOMEVENTS_H
