7bd82fe51e4a273883d0b285049d63b0e36f033b
Lucka12 <78818795+Lucka12@users.noreply.github.com>
2021-11-24 00:33:30 +0100

    Added comments

--- TMOJin17/TMOJin17.cpp
+++ TMOJin17/TMOJin17.cpp
@@ -3,7 +3,7 @@
 *                       Brno University of Technology                          *
 *                       CPhoto@FIT                                             *
 *                                                                              *
-*                       Tone Mapping Studio	                                   *
+*                       Tone Mapping Studio	                               *
 *                                                                              *
 *                       Bachelor thesis                                        *
 *                       Author: Filip Brezna [xbrezn00 AT stud.fit.vutbr.cz]   *
@@ -47,10 +47,10 @@ using namespace cv;
 #define OMEGA_SIZE 9
 
 
-unsigned int IMAGE_WIDTH; //global variable for width of loaded image
-unsigned int IMAGE_HEIGHT; //global variable for height of loaded image
+unsigned int IMAGE_WIDTH; /** global variable for width of loaded image */
+unsigned int IMAGE_HEIGHT; /** global variable for height of loaded image */
 
-//prototype of function used in class under this
+/** prototype of function used in class under this */
 double contrastLossComputation(unsigned int *PixNumberInClusters, Mat centersLAB, Mat centersRGB, Mat centersGray, int centersSize, arma::mat Omega, arma::umat ShuffledIndex);
 
 
@@ -76,10 +76,10 @@ public:
  	void setOldContrastLoss(double MyOldContrastLoss);
  	void setContrastLoss(double MyContrastLoss);
 
- 	//9 indexes of array for shuffling.
+ 	/** 9 indexes of array for shuffling. */
 	void Shuffle(void) {ShuffledIndex = arma::shuffle(arma::uvec("0 1 2 3 4 5 6 7 8"));}
 
-	//function that computes new Omega values in variable substitute coordinates
+	/** function that computes new Omega values in variable substitute coordinates */
 	double Evaluate (const arma::mat &coordinates)
 	{
 		contrastLoss = contrastLossComputation(PixNumberInClusters, CentersLAB, CentersRGB, CentersGray, CentersSize, coordinates, ShuffledIndex);
@@ -88,11 +88,11 @@ public:
 
 	void Gradient (const arma::mat &coordinates, arma::mat &gradient)
 	{
-		//mlpack:: already deleted Gradient in this moment
+		/** mlpack:: already deleted Gradient in this moment */
 		if (MaxIter >= 1000)
 			return;
 	
-		//check changes 
+		/** check changes  */
 		for (unsigned int i = 0; i < OMEGA_SIZE; ++i)
 			gradient[ShuffledIndex[i]] = (oldContrastLoss - contrastLoss) / (oldCoordinates[ShuffledIndex[i]] - coordinates[ShuffledIndex[i]]);
 
@@ -100,7 +100,7 @@ public:
 
 	}
 
-	//function that calls other functions from this class AdamClassType
+	/** function that calls other functions from this class AdamClassType */
 	double EvaluateWithGradient (const arma::mat &coordinates, const size_t begin, arma::mat &gradient, const size_t batchSize)
 	{
 		Shuffle();
@@ -115,7 +115,10 @@ public:
 
 };
 
-//functions for setting starting values of variables for AdamClassType
+
+/**
+ * @brief functions for setting starting values of variables for AdamClassType
+ */
 void AdamClassType::setPixNumberInCluster(unsigned int * MyPixNumberInClusters)
 {
 	PixNumberInClusters = MyPixNumberInClusters;
@@ -163,9 +166,9 @@ void AdamClassType::setContrastLoss(double MyContrastLoss)
 	contrastLoss = MyContrastLoss;
 }
 
-/* --------------------------------------------------------------------------- *
- * Constructor serves for describing a technique and input parameters          *
- * --------------------------------------------------------------------------- */
+/**
+  *  @brief Constructor
+  */
 TMOJin17::TMOJin17()
 {
 	SetName(L"Jin17");
@@ -180,6 +183,9 @@ TMOJin17::TMOJin17()
 
 }
 
+/**
+  *  @brief Destructor
+  */
 TMOJin17::~TMOJin17()
 {
 }
@@ -187,7 +193,7 @@ TMOJin17::~TMOJin17()
 unsigned int SignFunction(Mat centersRGB, int x, int y)
 {
 
-	//if r_x >= r_y AND g_x >= g_y AND b_x >= b_y
+	/** if r_x >= r_y AND g_x >= g_y AND b_x >= b_y */
 	if  (
 			//SHIFT_TO_L == SHIFT_TO_R
 			( centersRGB.at <float> (x, SHIFT_TO_L) >= centersRGB.at <float> (y, SHIFT_TO_L) ) &&
@@ -198,7 +204,7 @@ unsigned int SignFunction(Mat centersRGB, int x, int y)
 		)
 		return 1;
 
-	//if r_x < r_y AND g_x < g_y AND b_x < b_y
+	/** if r_x < r_y AND g_x < g_y AND b_x < b_y */
 	else if
 		(
 			//SHIFT_TO_L == SHIFT_TO_R
@@ -215,17 +221,22 @@ unsigned int SignFunction(Mat centersRGB, int x, int y)
 }
 
 
-//color conversion from CIELab to RGB
-//Always must go through XYZ color space
-//i am not author of this function: inpired by
-//https://stackoverflow.com/questions/9372626/converting-lab-values-to-rgb-values-in-opencv?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+/**
+ * @brief color conversion from CIELab to RGB
+ * Always must go through XYZ color space
+ * I am not author of this function: inpired by
+ * https://stackoverflow.com/questions/9372626/converting-lab-values-to-rgb-values-in-opencv?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+ * 
+ * @param srcLab 
+ * @param dstRGB 
+ */
 void convertLAB2RGB(Mat srcLab, Mat &dstRGB)
 {
 	float L, a, b;
 	float X, Y, Z;
 	float R, G, B;
 
-	// Lab -> normalized XYZ (X,Y,Z are all in 0...1)
+	/** Lab -> normalized XYZ (X,Y,Z are all in 0...1) */
 	for (unsigned int i = 0; i < srcLab.size().height; ++i)
 	{
 		L = srcLab.at <float> (i, SHIFT_TO_L);
@@ -241,13 +252,13 @@ void convertLAB2RGB(Mat srcLab, Mat &dstRGB)
 		Y = L > 8.0 ? Y * Y * Y : L * (27.0/24389.0);
 		Z = Z > 6.0/29.0 ? Z * Z * Z : Z * (108.0/841.0) - 432.0/24389.0;
 
-		// normalized XYZ -> linear sRGB (in 0...1)
+		/** normalized XYZ -> linear sRGB (in 0...1) */
 
 		R = X * (1219569.0/395920.0)     + Y * (-608687.0/395920.0)    + Z * (-107481.0/197960.0);
 		G = X * (-80960619.0/87888100.0) + Y * (82435961.0/43944050.0) + Z * (3976797.0/87888100.0);
 		B = X * (93813.0/1774030.0)      + Y * (-180961.0/887015.0)    + Z * (107481.0/93370.0);
 
-		// linear sRGB -> gamma-compressed sRGB (in 0...1)
+		/** linear sRGB -> gamma-compressed sRGB (in 0...1) */
 
 		R = R > 0.0031308 ? pow(R, 1.0 / 2.4) * 1.055 - 0.055 : R * 12.92;
 		G = G > 0.0031308 ? pow(G, 1.0 / 2.4) * 1.055 - 0.055 : G * 12.92;
@@ -261,8 +272,16 @@ void convertLAB2RGB(Mat srcLab, Mat &dstRGB)
 
 }
 
-
-//decolorization method via order set
+/**
+ * @brief Decalorization method via order set
+ * 
+ * @param sourceImageRGB 
+ * @param destImageGray 
+ * @param Mat_height 
+ * @param Mat_width 
+ * @param Omega 
+ * @param ShuffledIndex 
+ */
 void colorToGrayscaleConversion(Mat sourceImageRGB, Mat &destImageGray, int Mat_height, int Mat_width, arma::mat Omega, arma::umat ShuffledIndex)
 {
 
@@ -285,15 +304,15 @@ void colorToGrayscaleConversion(Mat sourceImageRGB, Mat &destImageGray, int Mat_
 				
 						for (unsigned int col = 0; col < Mat_width; ++col)
 						{
-							//taking all RGB values for one pixel and computing new ones to destination image
+							/** taking all RGB values for one pixel and computing new ones to destination image */
 							sourceImageRGB_r = sourceImageRGB.at <float> (col + row * Mat_width, SHIFT_TO_L);
 							sourceImageRGB_g = sourceImageRGB.at <float> (col + row * Mat_width, SHIFT_TO_A);
 							sourceImageRGB_b = sourceImageRGB.at <float> (col + row * Mat_width, SHIFT_TO_B);
 
-							//multiplying values with computed weight coefficients
+							/** multiplying values with computed weight coefficients */
 							result = Omega[ShuffledIndex[Omega_array_index]] * pow(sourceImageRGB_r, r) * pow(sourceImageRGB_g, g) * pow(sourceImageRGB_b, b);
 
-							//actualization of destination image for current pixel
+							/** actualization of destination image for current pixel */
 							destImageGray.at <float> (row, col) += result;
 
 						}
@@ -307,7 +326,19 @@ void colorToGrayscaleConversion(Mat sourceImageRGB, Mat &destImageGray, int Mat_
 
 }
 
-//function that computes Contrast Loss between lab and grayscale. We are looking for minimum returned double with Adam Gradient Descent solver
+/**
+ * @brief Function that computes Contrast Loss between lab and grayscale
+ * We are looking for minimum terurned doutle Adam Gradient Descent solver
+ * 
+ * @param PixNumberInClusters 
+ * @param centersLAB 
+ * @param centersRGB 
+ * @param centersGray 
+ * @param centersSize 
+ * @param Omega 
+ * @param ShuffledIndex 
+ * @return minimum  
+ */
 double contrastLossComputation(unsigned int *PixNumberInClusters, Mat centersLAB, Mat centersRGB, Mat centersGray, int centersSize, arma::mat Omega, arma::umat ShuffledIndex)
 {
 	double weightClusterPair;
@@ -319,7 +350,7 @@ double contrastLossComputation(unsigned int *PixNumberInClusters, Mat centersLAB
 
 	colorToGrayscaleConversion(centersLAB, centersGray, centersLAB.size().height, 1, Omega, ShuffledIndex);
 
-	//main cycle for Contrast Loss computation
+	/** main cycle for Contrast Loss computation */
 	for (unsigned int Ci = 0; Ci < centersSize; ++Ci)
 	{
 		for (unsigned int Cj = 0; Cj < centersSize; ++Cj)
@@ -335,7 +366,7 @@ double contrastLossComputation(unsigned int *PixNumberInClusters, Mat centersLAB
 			else if (signFunReturned == 3)
 				midStepCalculation = std::abs(midStepCalculation);
 
-			//second square root for this three channels
+			/** second square root for this three channels */
 			midStepCalculation -= sqrt(
 				//(L_Ci - L_Cj)^2 +
 				pow( (centersLAB.at <float> (Ci, SHIFT_TO_L) - (centersLAB.at <float> (Cj, SHIFT_TO_L)) ), 2) +
@@ -358,26 +389,26 @@ double contrastLossComputation(unsigned int *PixNumberInClusters, Mat centersLAB
 }
 
 
-/* --------------------------------------------------------------------------- *
- * This overloaded function is an implementation of your tone mapping operator *
- * --------------------------------------------------------------------------- */
+/**
+ * @brief Color to grayscale conversion
+ */
 int TMOJin17::Transform()
 {
 	// Source image is stored in local parameter pSrc
-	pSrc->Convert(TMO_LAB);								// This is format of CIELab
+	pSrc->Convert(TMO_LAB);								/** This is format of CIELab */
 	// Destination image is in pDst
-	pDst->Convert(TMO_LAB);								// This is format of CIELab
+	pDst->Convert(TMO_LAB);								/** This is format of CIELab */
 
-	double *sourceImage = pSrc->GetData();				// You can work at low level data
-	double *destinationImage = pDst->GetData();			// Data are stored in form of array 
-														// of three doubles representing
-														// three colour components
+	double *sourceImage = pSrc->GetData();				/** You can work at low level data */
+	double *destinationImage = pDst->GetData();			/** Data are stored in form of array 
+													      * of three doubles representing
+														  * three colour components */
 
-	//saving values of image width and height to global variables
+	/** saving values of image width and height to global variables */
 	IMAGE_WIDTH = pSrc->GetWidth();
 	IMAGE_HEIGHT = pSrc->GetHeight();
 
-	//clear destination image, we need that when working with video frames
+	/** clear destination image, we need that when working with video frames */
 	for (unsigned int y = 0; y < IMAGE_HEIGHT; ++y)
 	{
 		pSrc->ProgressBar(y, pSrc->GetHeight());
@@ -396,29 +427,29 @@ int TMOJin17::Transform()
 
 	Mat sourceImage_2D (IMAGE_HEIGHT * IMAGE_WIDTH, CIELAB_NUM_CHANNELS, CV_32F);
 
-	//go through all image pixels
+	/** go through all image pixels */
 	for (row = 0; row < IMAGE_HEIGHT; ++row)
 	{
 		for (col = 0; col < IMAGE_WIDTH; ++col)
 		{
-			//we copy and transform source image into 2D picture [row and col][color channel]
+			/** we copy and transform source image into 2D picture [row and col][color channel] */
 			for (CIELab_channel = 0; CIELab_channel < CIELAB_NUM_CHANNELS; ++CIELab_channel)
 				sourceImage_2D.at <float> (row + col * IMAGE_HEIGHT, CIELab_channel) = *(sourceImage + ((col + IMAGE_WIDTH  * row ) * CIELAB_NUM_CHANNELS) + CIELab_channel);	
 		
 		}
 	}		
 
-	// Flag to specify the number of times the algorithm is executed using different initial labellings.
-	//The algorithm returns the labels that yield the best compactness (see the last function parameter).
+	/** Flag to specify the number of times the algorithm is executed using different initial labellings.
+	 * The algorithm returns the labels that yield the best compactness (see the last function parameter). */
 	int attempts = 5;
 	
-	//Input/output integer array that stores the cluster indices for every sample.
+	/** Input/output integer array that stores the cluster indices for every sample. */
 	Mat labelsLAB, labelsGray;
 
-	//Output matrix of the cluster centers, one row per each cluster center.
+	/** Output matrix of the cluster centers, one row per each cluster center. */
 	Mat centersLAB;
 
-	//function for kmeans clustering. Image color reduction. For faster computations and memory save up
+	/** function for kmeans clustering. Image color reduction. For faster computations and memory save up */
 	kmeans(sourceImage_2D, K_clusters, labelsLAB, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centersLAB);
 
 
@@ -430,18 +461,18 @@ int TMOJin17::Transform()
 	int centersSize = centersLAB.size().height;
 
 	unsigned int *PixNumberInClusters = new unsigned int [centersSize];
-	//fill array PixNumberInClusters with zeroes - starting value
+	/** fill array PixNumberInClusters with zeroes - starting value */
 	std::fill_n(PixNumberInClusters, centersSize, 0);
 
 	unsigned int indexToArray;
 	
-	//computation for number of pixels in specific clusters
+	/** computation for number of pixels in specific clusters */
 	for (unsigned int i = 0; i < labelsSize; ++i)
 	{
-		//find value in labels, take it as index
+		/** find value in labels, take it as index */
 		indexToArray = labelsLAB.at <int> (i, 0);
 		
-		//on that index increment number of occurrence
+		/** on that index increment number of occurrence */
 		PixNumberInClusters[indexToArray] += 1;
 	}
 
@@ -451,10 +482,10 @@ int TMOJin17::Transform()
 	Mat centersGray = Mat::zeros(centersLAB.size().height, 1, CV_32F);
 
 
-	//					w_b  w_b^2  w_g  w_gb w_g^2 w_r w_rb w_rg w_r^2
+	             /**	w_b  w_b^2  w_g  w_gb w_g^2 w_r w_rb w_rg w_r^2 */
 	arma::mat Omega = {0.33, 0.01, 0.33, 0.03, 0.02, 0.33, 0.05, 0.04, 0.06};
 
-	//Initialization of Adam Gradient Descent with parameters StepSize and MaxIterations
+	/** Initialization of Adam Gradient Descent with parameters StepSize and MaxIterations */
 	mlpack::optimization::AdamType<mlpack::optimization::AdamUpdate> AdamGradientDescent;
 
 	double LearnRate = 0.0005;
@@ -470,7 +501,7 @@ int TMOJin17::Transform()
 	double &Tolerance = AdamGradientDescent.Tolerance();
 	Tolerance = 0.0;
 
-	//Instanciation of class AdamClassType
+	/** Instanciation of class AdamClassType */
 	AdamClassType AdamClassTypeInit;
 	AdamClassTypeInit.setPixNumberInCluster(PixNumberInClusters);
 	AdamClassTypeInit.setCentersLAB(centersLAB);
@@ -487,7 +518,7 @@ int TMOJin17::Transform()
 
 	//###Final decolorization###
 
-	//convert color spaces back to RGB
+	/** convert color spaces back to RGB */
 	pSrc->Convert(TMO_RGB);
 	pDst->Convert(TMO_RGB);
 
@@ -495,7 +526,7 @@ int TMOJin17::Transform()
 	double result;
 	unsigned int Omega_array_index = 0;
 
-	//myfile << "Omega" << Omega << std::endl;
+	// myfile << "Omega" << Omega << std::endl; 
 
 	for (unsigned int r = 0; r <= 2; ++r)
 	{
@@ -503,29 +534,29 @@ int TMOJin17::Transform()
 		{
 			for (unsigned int b = 0; b <= 2; ++b)
 			{
-				//it takes exactly what we expect in Omega.
-				//If exponent for color is 2, other colors have zero etc.
+				/** it takes exactly what we expect in Omega.
+				 * If exponent for color is 2, other colors have zero etc. */
 				if ( ((r + g + b) <= 2) &&  ((r + g + b) > 0))
 				{
-					//setting pointer to source and destination image again on starting position.
+					/** setting pointer to source and destination image again on starting position. */
 					destinationImage = pDst->GetData();
 					sourceImage = pSrc->GetData();
 
-					//going through all pixels, first x - cols then y - rows
+					/** going through all pixels, first x - cols then y - rows */
 					for (unsigned int y = 0; y < IMAGE_HEIGHT; ++y)
 					{
 						pSrc->ProgressBar(y, pSrc->GetHeight());
 						for (unsigned int x = 0; x < IMAGE_WIDTH; ++x)
 						{
-							//taking all RGB values for one pixel and computing new ones to destination image
+							/** taking all RGB values for one pixel and computing new ones to destination image */
 							sourceImage_r = *(sourceImage++);
 							sourceImage_g = *(sourceImage++);
 							sourceImage_b = *(sourceImage++);
 
-							//multiplying values with computed weight coefficients
+							/** multiplying values with computed weight coefficients */
 							result = Omega[AdamClassTypeInit.ShuffledIndex[Omega_array_index]] * pow(sourceImage_r, r) * pow(sourceImage_g, g) * pow(sourceImage_b, b);
 
-							//actualization of destination image for current pixel
+							/** actualization of destination image for current pixel */
 							*(destinationImage++) += result;
 							*(destinationImage++) += result;
 							*(destinationImage++) += result;
@@ -533,7 +564,7 @@ int TMOJin17::Transform()
 						}
 					}
 
-					//color recomputed for all pixels, lets take another color combination from Omega
+					/** color recomputed for all pixels, lets take another color combination from Omega */
 					Omega_array_index++;
 						
 				}
@@ -541,7 +572,7 @@ int TMOJin17::Transform()
 		}
 	}
 
- 	//dealocation of array for storage
+ 	/** dealocation of array for storage */
 	delete[] PixNumberInClusters;
 
 	return 0;
