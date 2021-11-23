2a1e9ce799e8c5bc02b9f3519ec4dab72f5bc17c
davidchocholaty <david.chocholaty@seznam.cz>
2021-11-23 01:47:27 +0100

    Fix luminance calculation

--- tmolib/TMOImage.h
+++ tmolib/TMOImage.h
@@ -177,9 +177,9 @@ public:
 	virtual void GetStatisticsSquareRange(int x, int y, int size = 0);
 	virtual double GetLuminanceYxy(int x, int y, int r = 0);
 	virtual double SetLuminance(int x, int y, double L);
+	virtual void CalculateLuminance(double &maximum, double &average, unsigned int height, unsigned int width);
 	virtual int GetMinMaxAvg(double *minimum, double *maximum, double *average);
-	virtual int GetMinMaxAvgLog10(double *minimum, double *maximum, double *average);
-	virtual int GetMinMaxAvgWorldAdapt (double *minimum, double *maximum, double *average);
+	virtual int GetMinMaxAvgLog10(double *minimum, double *maximum, double *average);	
 	
 	virtual int GetDimensions(int *x, int *y);
 	virtual int SetDimensions(int x, int y) {iWidth = x; iHeight = y; return 0;}
@@ -187,8 +187,7 @@ public:
 	virtual const char* GetFilename() {return(pName);};
 	virtual int SetFilename(const char* filename);
 	virtual int CorrectGamma(double gamma); 		
-	virtual int CorrectGammaYxy(double gamma); //just for testing, not reasonable
-	virtual int RecCorrectGamma (double gamma);
+	virtual int CorrectGammaYxy(double gamma); //just for testing, not reasonable	
 	virtual int CenterWeight (int centerX, int centerY, float kernel, double *average);
 	
 	virtual int SetData(double* &newData);
