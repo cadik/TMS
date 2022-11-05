8a484257265d9440716c57e97934a4ed0cd38226
Matthewlele <bicanovsky.matus@gmail.com>
2022-11-05 18:42:16 +0100

    neighbour finding implemented

--- TMOYee03/TMOYee03.cpp
+++ TMOYee03/TMOYee03.cpp
@@ -59,6 +59,8 @@ unsigned int IMAGE_HEIGHT;
 unsigned int IMAGE_WIDTH;
 double bin_size = 0.0;
 double MinimumImageLuminance = 0.0;
+double stonits = 0.0;
+int GroupNumber = 0;
 
 struct Point{
    unsigned short x,y;
@@ -80,6 +82,10 @@ typedef std::vector<Groups> Layers;
 
 typedef vector< vector<int> > PixelMatrix;
 
+typedef vector< vector<double> > AdaptationMatrix;
+
+
+
 Groups CategoryGroups;
 
 TMOYee03::~TMOYee03()
@@ -93,7 +99,7 @@ double pixelCategory(double *image, int x, int y)
    pG = *(image+((y*IMAGE_WIDTH*3)+(x*3))+1);
    pB = *(image+((y*IMAGE_WIDTH*3)+(x*3))+2);
    double category = ((log10(rgb2luminance(pR,pG,pB))-log10(MinimumImageLuminance))/bin_size);
-   category = std::round(category * 10.0) / 10.0;
+   category = std::round(category * 1.0) / 1.0;
    return category;
 }
 
@@ -106,13 +112,14 @@ bool isValid(double *image, int x, int y, double category, PixelMatrix& pixels)
    return true;
 }
 
-void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels)
+void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels, AdaptationMatrix& pixelCategories)
 {
    vector<pair<int, int>> queue;
    pair<int, int> p(x,y);
    queue.push_back(p);
 
    pixels[x][y] = 1;
+   pixelCategories[x][y] = GroupNumber;
 
    Group_Record group;
    group.Count = 0;
@@ -128,7 +135,12 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
    pR = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3)));
    pG = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+1);
    pB = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+2);
-   group.Sum += rgb2luminance(pR,pG,pB);
+   double lum = rgb2luminance(pR,pG,pB);
+   if(lum == 0)
+   {
+      lum = MinimumImageLuminance/stonits;
+   }
+   group.Sum += log10(lum+stonits);
    group.Count += 1;
 
    while(queue.size() > 0)
@@ -142,6 +154,7 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
       if(isValid(image, posX+1, posY, category, pixels))
       {
          pixels[posX+1][posY] = 1;
+         pixelCategories[posX+1][posY] = GroupNumber;
          point.x = posX+1;
          point.y = posY;
 
@@ -150,7 +163,12 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
          pR = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3)));
          pG = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+1);
          pB = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+2);
-         group.Sum += rgb2luminance(pR,pG,pB);
+         double lum = rgb2luminance(pR,pG,pB);
+         if(lum == 0)
+         {
+            lum = MinimumImageLuminance/stonits;
+         }
+         group.Sum += log10(lum+stonits);
          group.Count += 1;
 
          p.first = posX+1;
@@ -161,6 +179,7 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
       if(isValid(image, posX-1, posY, category, pixels))
       {
          pixels[posX-1][posY] = 1;
+         pixelCategories[posX-1][posY] = GroupNumber;
          point.x = posX-1;
          point.y = posY;
 
@@ -169,7 +188,12 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
          pR = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3)));
          pG = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+1);
          pB = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+2);
-         group.Sum += rgb2luminance(pR,pG,pB);
+         double lum = rgb2luminance(pR,pG,pB);
+         if(lum == 0)
+         {
+            lum = MinimumImageLuminance/stonits;
+         }
+         group.Sum += log10(lum+stonits);
          group.Count += 1;
          
          p.first = posX-1;
@@ -179,6 +203,7 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
       if(isValid(image, posX, posY+1, category, pixels))
       {
          pixels[posX][posY+1] = 1;
+         pixelCategories[posX][posY+1] = GroupNumber;
          point.x = posX;
          point.y = posY+1;
 
@@ -187,7 +212,12 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
          pR = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3)));
          pG = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+1);
          pB = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+2);
-         group.Sum += rgb2luminance(pR,pG,pB);
+         double lum = rgb2luminance(pR,pG,pB);
+         if(lum == 0)
+         {
+            lum = MinimumImageLuminance/stonits;
+         }
+         group.Sum += log10(lum+stonits);
          group.Count += 1;
          
          p.first = posX;
@@ -197,6 +227,7 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
       if(isValid(image, posX, posY-1, category, pixels))
       {
          pixels[posX][posY-1] = 1;
+         pixelCategories[posX][posY-1] = GroupNumber;
          point.x = posX;
          point.y = posY-1;
 
@@ -205,7 +236,12 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
          pR = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3)));
          pG = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+1);
          pB = *(image+((point.y*IMAGE_WIDTH*3)+(point.x*3))+2);
-         group.Sum += rgb2luminance(pR,pG,pB);
+         double lum = rgb2luminance(pR,pG,pB);
+         if(lum == 0)
+         {
+            lum = MinimumImageLuminance/stonits;
+         }
+         group.Sum += log10(lum+stonits);
          group.Count += 1;
          
          p.first = posX;
@@ -215,9 +251,148 @@ void floodFill(double *image, int x, int y, double category, PixelMatrix& pixels
 
    }
    CategoryGroups.push_back(group);
+   GroupNumber += 1;
 
 }
 
+/*void AssignNeighbour(int curr_x, int curr_y, int prev_x, int prev_y, AdaptationMatrix& pixelCategories)
+{
+   int current_group = -1;
+   int prev_group = -1;
+   for(int i = 0; i < CategoryGroups.size();i++)
+   {
+      if(pixelCategories[curr_x][curr_y] == CategoryGroups[i].category)
+      {
+         current_group = i;
+      }
+      if(pixelCategories[prev_x][prev_y] == CategoryGroups[i].category)
+      {
+         prev_group = i;
+      }
+      if(prev_group > -1 && current_group > -1){
+         break;
+      }
+   }
+   if(!(std::find(CategoryGroups[current_group].Neighbours.begin(), CategoryGroups[current_group].Neighbours.end(), prev_group) != CategoryGroups[current_group].Neighbours.end())){
+      CategoryGroups[current_group].Neighbours.push_back(prev_group);
+   }
+   if(!(std::find(CategoryGroups[prev_group].Neighbours.begin(), CategoryGroups[prev_group].Neighbours.end(), current_group) != CategoryGroups[prev_group].Neighbours.end())){
+      CategoryGroups[prev_group].Neighbours.push_back(current_group);
+   }
+   
+
+}*/
+
+void GroupNeighbours(double *image, int x, int y, double category, PixelMatrix& pixels, AdaptationMatrix& pixelCategories)
+{
+   vector<pair<int, int>> queue;
+   pair<int, int> p(x,y);
+   queue.push_back(p);
+
+   pixels[x][y] = 1;
+
+  
+   //Groups.push_back(Group_Record.Members.push_back(point));
+
+   while(queue.size() > 0)
+   {
+      pair<int,int> currPixel = queue[queue.size() - 1];
+      queue.pop_back();
+
+      int posX = currPixel.first;
+      int posY = currPixel.second;
+
+      if(isValid(image, posX+1, posY, category, pixels))
+      {
+         pixels[posX+1][posY] = 1;
+         
+         p.first = posX+1;
+         p.second = posY;
+         queue.push_back(p);
+      }
+      if(!(isValid(image, posX+1, posY, category, pixels)))
+      {
+         if(posX+1>=0 && posX+1< IMAGE_WIDTH && posY>=0 && posY<IMAGE_HEIGHT)
+         {
+            pixels[posX+1][posY] = 1;
+            if(!(std::find(CategoryGroups[pixelCategories[posX+1][posY]].Neighbours.begin(), CategoryGroups[pixelCategories[posX+1][posY]].Neighbours.end(), pixelCategories[posX][posY]) != CategoryGroups[pixelCategories[posX+1][posY]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX+1][posY]].Neighbours.push_back(pixelCategories[posX][posY]);
+            }
+            if(!(std::find(CategoryGroups[pixelCategories[posX][posY]].Neighbours.begin(), CategoryGroups[pixelCategories[posX][posY]].Neighbours.end(), pixelCategories[posX+1][posY]) != CategoryGroups[pixelCategories[posX][posY]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX][posY]].Neighbours.push_back(pixelCategories[posX+1][posY]);
+            }
+         }
+      }
+
+      if(isValid(image, posX-1, posY, category, pixels))
+      {
+         pixels[posX-1][posY] = 1;
+         
+         p.first = posX-1;
+         p.second = posY;
+         queue.push_back(p);
+      }
+      if(!(isValid(image, posX-1, posY, category, pixels)))
+      {
+         if(posX-1>=0 && posX-1< IMAGE_WIDTH && posY>=0 && posY<IMAGE_HEIGHT)
+         {
+            pixels[posX-1][posY] = 1;
+            if(!(std::find(CategoryGroups[pixelCategories[posX-1][posY]].Neighbours.begin(), CategoryGroups[pixelCategories[posX-1][posY]].Neighbours.end(), pixelCategories[posX][posY]) != CategoryGroups[pixelCategories[posX-1][posY]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX-1][posY]].Neighbours.push_back(pixelCategories[posX][posY]);
+            }
+            if(!(std::find(CategoryGroups[pixelCategories[posX][posY]].Neighbours.begin(), CategoryGroups[pixelCategories[posX][posY]].Neighbours.end(), pixelCategories[posX-1][posY]) != CategoryGroups[pixelCategories[posX][posY]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX][posY]].Neighbours.push_back(pixelCategories[posX-1][posY]);
+            }
+         }
+         
+      }
+      if(isValid(image, posX, posY+1, category, pixels))
+      {
+         pixels[posX][posY+1] = 1;
+         
+         p.first = posX;
+         p.second = posY+1;
+         queue.push_back(p);
+      }
+      if(!(isValid(image, posX, posY+1, category, pixels)))
+      {
+         if(posX>=0 && posX< IMAGE_WIDTH && posY+1>=0 && posY+1<IMAGE_HEIGHT)
+         {
+            pixels[posX][posY+1] = 1;
+            if(!(std::find(CategoryGroups[pixelCategories[posX][posY+1]].Neighbours.begin(), CategoryGroups[pixelCategories[posX][posY+1]].Neighbours.end(), pixelCategories[posX][posY]) != CategoryGroups[pixelCategories[posX][posY+1]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX][posY+1]].Neighbours.push_back(pixelCategories[posX][posY]);
+            }
+            if(!(std::find(CategoryGroups[pixelCategories[posX][posY]].Neighbours.begin(), CategoryGroups[pixelCategories[posX][posY]].Neighbours.end(), pixelCategories[posX][posY+1]) != CategoryGroups[pixelCategories[posX][posY]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX][posY]].Neighbours.push_back(pixelCategories[posX][posY+1]);
+            }
+         }
+         
+      }
+      if(isValid(image, posX, posY-1, category, pixels))
+      {
+         pixels[posX][posY-1] = 1;
+         
+         p.first = posX;
+         p.second = posY-1;
+         queue.push_back(p);
+      }
+      if(!(isValid(image, posX, posY-1, category, pixels)))
+      {
+         if(posX>=0 && posX< IMAGE_WIDTH && posY-1>=0 && posY-1<IMAGE_HEIGHT)
+         {
+            pixels[posX][posY-1] = 1;
+            if(!(std::find(CategoryGroups[pixelCategories[posX][posY-1]].Neighbours.begin(), CategoryGroups[pixelCategories[posX][posY-1]].Neighbours.end(), pixelCategories[posX][posY]) != CategoryGroups[pixelCategories[posX][posY-1]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX][posY-1]].Neighbours.push_back(pixelCategories[posX][posY]);
+            }
+            if(!(std::find(CategoryGroups[pixelCategories[posX][posY]].Neighbours.begin(), CategoryGroups[pixelCategories[posX][posY]].Neighbours.end(), pixelCategories[posX][posY-1]) != CategoryGroups[pixelCategories[posX][posY]].Neighbours.end())){
+               CategoryGroups[pixelCategories[posX][posY]].Neighbours.push_back(pixelCategories[posX][posY-1]);
+            }
+         }
+         
+      }
+
+   }
+}
 
 /* --------------------------------------------------------------------------- *
  * This overloaded function is an implementation of your tone mapping operator *
@@ -237,15 +412,16 @@ int TMOYee03::Transform()
    IMAGE_HEIGHT = pSrc->GetHeight();
    IMAGE_WIDTH = pSrc->GetWidth();
 
-   double stonits = pSrc->GetStonits();
+   stonits = pSrc->GetStonits();
    double MinimalImageLuminance = 0., MaximalImageLuminance = 0., AverageImageLuminance = 0.;
    pSrc->GetMinMaxAvg(&MinimalImageLuminance, &MaximalImageLuminance, &AverageImageLuminance);
    MinimumImageLuminance = MinimalImageLuminance*stonits;
    MaximalImageLuminance = MaximalImageLuminance*stonits;
-
-
+   AdaptationMatrix adaptationPixels(IMAGE_WIDTH, vector<double>(IMAGE_HEIGHT,0));
+   AdaptationMatrix pixelCategories(IMAGE_WIDTH, vector<double>(IMAGE_HEIGHT,0));
    PixelMatrix pixels(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT,0));
    int groups = 0;
+   GroupNumber = 0;
    for(int j = 0; j < IMAGE_HEIGHT; j++)
    {
       for(int i = 0; i < IMAGE_WIDTH; i++)
@@ -254,36 +430,60 @@ int TMOYee03::Transform()
          {
             double category = pixelCategory(pSourceData, i, j);
             //fprintf(stderr, "Category : %g of pixel x:%d y:%d\n",category,i,j);
-            floodFill(pSourceData, i, j, category, pixels);
+            floodFill(pSourceData, i, j, category, pixels, pixelCategories);
             groups++;
          }
       }
    }
    fprintf(stderr,"Groups after grouping %d\n",CategoryGroups.size());
-   int pixelAmountCount = 0;
-   int pixelAmountMembers = 0;
-   for(int i=0; i < CategoryGroups.size();i++)
+
+   PixelMatrix MembersPixels(IMAGE_WIDTH, vector<int>(IMAGE_HEIGHT,0));
+   for(int j = 0; j < IMAGE_HEIGHT; j++)
    {
-      fprintf(stderr,"Group num: %d , amount of pixels %d, sum: %g, count: %d\n",i,CategoryGroups[i].Memebers.size(),CategoryGroups[i].Sum,CategoryGroups[i].Count);
-      pixelAmountCount += CategoryGroups[i].Count;
-      pixelAmountMembers += CategoryGroups[i].Memebers.size();
+      for(int i = 0; i < IMAGE_WIDTH; i++)
+      {
+         if(MembersPixels[i][j] != 1)
+         {
+            fprintf(stderr,"pixel %d %d\n",i,j);
+            double category = pixelCategory(pSourceData, i, j);
+            //fprintf(stderr, "Category : %g of pixel x:%d y:%d\n",category,i,j);
+            GroupNeighbours(pSourceData, i, j, category, MembersPixels, pixelCategories);
+            groups++;
+         }
+      }
    }
 
-   fprintf(stderr,"PixelAmountCount : %d , PixelAmountMemebers : %d , Calculated pixels: %d\n",pixelAmountCount,pixelAmountMembers,IMAGE_HEIGHT*IMAGE_WIDTH);
+   
 
+   for(int i=0; i < CategoryGroups.size();i++)
+   {
+      fprintf(stderr,"Group num: %d group neighbours %d\n",i,CategoryGroups[i].Neighbours.size());
+      for(int j=0; j < CategoryGroups[i].Memebers.size();j++)
+      {
+         adaptationPixels[CategoryGroups[i].Memebers[j].x][CategoryGroups[i].Memebers[j].y] += CategoryGroups[i].Sum/CategoryGroups[i].Count;
+         //fprintf(stderr,"Pixel x:%d y:%d lum: %g\n",CategoryGroups[i].Memebers[j].x,CategoryGroups[i].Memebers[j].y, CategoryGroups[i].Sum/CategoryGroups[i].Count);
+      }
+      /*for(int k =0; k < CategoryGroups[i].Neighbours.size();k++)
+      {
+         list<unsigned short>::iterator it = CategoryGroups[i].Neighbours.begin();
+         advance(it, k);
+         fprintf(stderr,"%d ",*it);
+      }*/
+      //fprintf(stderr,"\n");
+   }
 
-
+   fprintf(stderr,"Stonits : %g\n",stonits);
    
    // Initialy images are in RGB format, but you can
 	// convert it into other format
 	//pSrc->Convert(TMO_Yxy); // This is format of Y as luminance
 	//pDst->Convert(TMO_Yxy); // x, y as color information
-   double AdaptationLuminance = 800.0;
+   //double aAdaptationLuminance = 800.0;
 	int j = 0;
-	for (j = 0; j < pSrc->GetHeight(); j++)
+	for (j = 0; j < pSrc->GetHeight(); j++) //
 	{
 		pSrc->ProgressBar(j, pSrc->GetHeight()); // You can provide progress bar
-		for (int i = 0; i < pSrc->GetWidth(); i++)
+		for (int i = 0; i < pSrc->GetWidth(); i++) //
 		{
 			//pR = *(pSourceData+((j*IMAGE_WIDTH*3)+(i*3)));
 			//pG = *(pSourceData+((j*IMAGE_WIDTH*3)+(i*3)+1));
@@ -291,19 +491,21 @@ int TMOYee03::Transform()
          pR = *pSourceData++;
 			pG = *pSourceData++;
 			pB = *pSourceData++;
-
+         
          //fprintf(stderr, "pixel %i,%i value R: %g G: %g B: %g",j,i,pR,pG,pB);
+         
+         
 
 			// Here you can use your transform
 			// expressions and techniques...
 			//pR *= dParameter; // Parameters can be used like
 							  // simple variables
-         double L_wa = cdm2ToLambert(AdaptationLuminance);
-         double L_w = cdm2ToLambert(rgb2luminance(pR, pG, pB));
+         double L_wa = cdm2ToLambert(adaptationPixels[i][j]*1000);
+         double L_w = cdm2ToLambert(rgb2luminance(pR, pG, pB)*stonits);
          double f_r = pR/L_w, f_g = pG/L_w, f_b = pB/L_w;
 
          double S_w = 100.0 + 10.0*log10(L_wa);
-         double R_w = 10.0*log10(L_wa/L_w);
+         double R_w = 10.0 * log10(L_wa/L_w);
 
          double L_dMax = cdm2ToLambert(MAX_DISPLAY_LUMINANCE);
          double L_da = cdm2ToLambert(DISPLAY_ADAPTATION_LUMINANCE);
