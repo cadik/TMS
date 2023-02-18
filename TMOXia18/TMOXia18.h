cf84c66b6591d02a2e8f466cf7455a90f77f9724
David Chocholaty <david.chocholaty@seznam.cz>
2023-02-18 21:33:03 +0100

    TMOXia18: add operator implementation
    This commit adds an implementation for the TMOXia18 operator
    that implements the Invertible Grayscale paper.

--- TMOXia18/TMOXia18.h
+++ TMOXia18/TMOXia18.h
@@ -1,5 +1,12 @@
 #include "TMO.h"
 
+#include <tensorflow/core/public/session.h>
+#include <tensorflow/core/protobuf/meta_graph.pb.h>
+
+
+using namespace std;
+using namespace tensorflow;
+
 class TMOXia18 : public TMO
 {
 public:
@@ -8,5 +15,45 @@ public:
 	virtual int Transform();
 
 protected:
-	TMODouble dParameter;
+    /*
+     * Run a color to grayscale conversion or grayscale to color prediction.
+     */
+    Status run(Tensor &image,
+               vector<Tensor> &result,
+               int direction,
+               Session* session);
+
+    /*
+     * The function fills the tensor with data from the source (pSrc).
+     */
+    void fillTensor(Tensor *t,
+                    int batch,
+                    int depth,
+                    int windowPositionY,
+                    int windowPositionX);
+
+    /*
+     * The function fills the destination (pDst) with data from the tensor.
+     * The tensor is obtained by prediction.
+     */
+    void fillOutput(Tensor *t,
+                    int batchY,
+                    int batchX,
+                    int batchCountY,
+                    int batchCountX);
+
+    /*
+     * Function handles the work with the tensor and prediction.
+     */
+    Status predict(Session* session);
+
+    /*
+     * The function loads the model for the prediction.
+     */
+    Session* loadModel();
+
+    /*
+     * Color to grayscale or grayscale to color direction.
+     */
+    TMOString direction;
 };
