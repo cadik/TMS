9717fb0e07fa6c16225f995c9b2ace764dc76845
Peter Zdravecký <pzdravec@redhat.com>
2024-02-17 09:30:28 +0100

    Implementation of Queiroz06 - Color to gray

--- TMOQueiroz06/TMOQueiroz06.h
+++ TMOQueiroz06/TMOQueiroz06.h
@@ -1,4 +1,13 @@
 #include "TMO.h"
+#include "wavelib.h"
+#include <opencv2/opencv.hpp>
+
+struct YCbCr
+{
+	double Y;
+	double Cb;
+	double Cr;
+};
 
 class TMOQueiroz06 : public TMO
 {
@@ -6,6 +15,8 @@ public:
 	TMOQueiroz06();
 	virtual ~TMOQueiroz06();
 	virtual int Transform();
+
+	YCbCr RGBToYCbCr(double fr, double fg, double fb);
 
 protected:
 	TMODouble dParameter;
