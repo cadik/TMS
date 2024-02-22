b836e7d808cd7fbd614335a57b72171aa7f42578
Peter Zdravecký <pzdravec@redhat.com>
2024-02-22 11:50:25 +0100

    Implementation of Liu15 - GcsDecolor2

--- TMOLiu15/TMOLiu15.h
+++ TMOLiu15/TMOLiu15.h
@@ -1,4 +1,10 @@
 #include "TMO.h"
+#include <opencv2/opencv.hpp>
+#include <algorithm>
+#include <random>
+#include <cmath>
+
+using namespace cv;
 
 class TMOLiu15 : public TMO
 {
@@ -6,7 +12,8 @@ public:
 	TMOLiu15();
 	virtual ~TMOLiu15();
 	virtual int Transform();
+	Mat wei();
 
 protected:
-	TMODouble dParameter;
+	TMODouble Lpp;
 };
