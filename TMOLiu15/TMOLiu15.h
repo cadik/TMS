e848503583aa2fd043792185efb9a8c35047dfe8
Peter Zdravecký <pzdravec@redhat.com>
2024-02-22 11:50:25 +0100

    Implementation of Liu15 - GcsDecolor2

--- TMOLiu15/TMOLiu15.h
+++ TMOLiu15/TMOLiu15.h
@@ -1,4 +1,21 @@
+/*******************************************************************************
+*                                                                              *
+*                         Brno University of Technology                        *
+*                       Faculty of Information Technology                      *
+*                                                                              *
+*          						   GcsDecolor2s                                *
+*                                                                              *
+*             Author: Peter Zdravecký [xzdrav00 AT stud.fit.vutbr.cz]          *
+*                                    Brno 2024                                 *
+*                                                                              *
+*******************************************************************************/
 #include "TMO.h"
+#include <opencv2/opencv.hpp>
+#include <algorithm>
+#include <random>
+#include <cmath>
+
+using namespace cv;
 
 class TMOLiu15 : public TMO
 {
@@ -6,7 +23,8 @@ public:
 	TMOLiu15();
 	virtual ~TMOLiu15();
 	virtual int Transform();
+	Mat wei();
 
 protected:
-	TMODouble dParameter;
+	TMODouble Lpp;
 };
