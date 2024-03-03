e1d1402f35c7f505fae7fe7710a82b488507364f
Peter Zdravecký <pzdravec@redhat.com>
2024-03-03 11:46:31 +0100

    Fix output value range

--- TMOQueiroz06/TMOQueiroz06.h
+++ TMOQueiroz06/TMOQueiroz06.h
@@ -1,7 +1,22 @@
+/*******************************************************************************
+*                                                                              *
+*                         Brno University of Technology                        *
+*                       Faculty of Information Technology                      *
+*                                                                              *
+*       Color to Gray and Back: Color Embedding Into Textured Gray Images      *
+*                                                                              *
+*             Author: Peter Zdravecký [xzdrav00 AT stud.fit.vutbr.cz]          *
+*                                    Brno 2024                                 *
+*                                                                              *
+*******************************************************************************/
 #include "TMO.h"
 #include "wavelib.h"
 #include <opencv2/opencv.hpp>
 
+/**
+ * @brief Structure for YCbCr color space
+ * 
+ */
 struct YCbCr
 {
 	double Y;
