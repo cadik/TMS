9ce751e90916570f95879c50a309cdd09bdc7cc9
Boris Strbak <borisstrbak@gmail.com>
2024-03-24 17:29:19 +0100

    fea(TMOTMONafchi17): implementation of transformer

---
+++ TMOTMONafchi17/TMOTMONafchi17.h
@@ -0,0 +1,13 @@
+#include "TMO.h"
+
+class TMOTMONafchi17 : public TMO
+{
+public:
+	TMOTMONafchi17();
+	virtual ~TMOTMONafchi17();
+	virtual int Transform();
+
+protected:
+	TMODouble dParameter;
+	TMOBool bParameter;
+};
