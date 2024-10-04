947a28b94d1b509121895f8f9e4f8015e4953ab3
Ludmila Kejcova <LudmilaKrejcova>
2024-10-04 20:28:11 +0200

    Added color temperature-based high-speed decolorization

---
+++ xkrejc85/xkrejc85.cpp
@@ -0,0 +1,77 @@
+/* --------------------------------------------------------------------------- *
+ * TMOYourOperatorName.cpp: implementation of the TMOYourOperatorName class.   *
+ * --------------------------------------------------------------------------- */
+
+#include "xkrejc85.h"
+
+/* --------------------------------------------------------------------------- *
+ * Constructor serves for describing a technique and input parameters          *
+ * --------------------------------------------------------------------------- */
+xkrejc85::xkrejc85()
+{
+	SetName(L"xkrejc85");					  // TODO - Insert operator name
+	SetDescription(L"Add your TMO description here"); // TODO - Insert description
+
+//	dParameter.SetName(L"ParameterName");				// TODO - Insert parameters names
+//	dParameter.SetDescription(L"ParameterDescription"); // TODO - Insert parameter descriptions
+//	dParameter.SetDefault(1);							// TODO - Add default values
+//	dParameter = 1.;
+//	 dParameter.SetRange(-1000.0, 1000.0); // TODO - Add acceptable range if needed
+//	this->Register(dParameter);
+}
+
+xkrejc85::~xkrejc85()
+{
+}
+
+/* --------------------------------------------------------------------------- *
+ * This overloaded function is an implementation of your tone mapping operator *
+ * --------------------------------------------------------------------------- */
+int xkrejc85::Transform()
+{
+	// Source image is stored in local parameter pSrc
+	// Destination image is in pDst
+
+	// Initialy images are in RGB format, but you can
+	// convert it into other format
+	//pSrc->Convert(TMO_Yxy); // This is format of Y as luminance
+	//pDst->Convert(TMO_Yxy); // x, y as color information
+
+	double *pSourceData = pSrc->GetData();		// You can work at low level data
+	double *pDestinationData = pDst->GetData(); // Data are stored in form of array
+												// of three doubles representing
+												// three colour components
+	//double pY, px, py;
+   double R, G, B, LWhite, LRG, LWarm, LCool, LB, L; 
+   double parameter = 0.75;
+
+	for (int j = 0; j < pSrc->GetHeight(); j++)
+	{
+		pSrc->ProgressBar(j, pSrc->GetHeight()); // You can provide progress bar
+		for (int i = 0; i < pSrc->GetWidth(); i++)
+		{
+			R = *pSourceData++;
+			G = *pSourceData++;
+			B = *pSourceData++;
+
+         LWhite = sqrt((R*R + G*G + B*B)/3);
+         LB = B;
+         LRG = sqrt(parameter*R*R + (1-parameter)*G*G);
+         LWarm = R/(R+G+B)*LRG + (1-R/(R+G+B))*LWhite;
+         LCool = (1-B/(R+G+B))*LB + B/(R+G+B)*LWhite;
+         L = sqrt(parameter*LWarm*LWarm + (1-parameter)*LCool*LCool);
+			// Here you can use your transform
+			// expressions and techniques...
+			//pY *= dParameter; // Parameters can be used like
+							  // simple variables
+
+			// and store results to the destination image
+			*pDestinationData++ = L;
+			*pDestinationData++ = L;
+			*pDestinationData++ = L;
+		}
+	}
+//	pSrc->ProgressBar(j, pSrc->GetHeight());
+	//pDst->Convert(TMO_RGB);
+	return 0; 
+}
---
+++ xkrejc85/xkrejc85.h
@@ -0,0 +1,12 @@
+#include <TMO.h>
+
+class xkrejc85 : public TMO
+{
+public:
+	xkrejc85();
+	virtual ~xkrejc85();
+	virtual int Transform() override;
+
+protected:
+	TMODouble dParameter;
+};
