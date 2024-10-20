0d8b8b2647f4152fa37c9451561b118e3e320ee3
findraj <jankofindra@gmail.com>
2024-10-20 18:59:04 +0000

    feat: beginngs of Mikamo 14 method

--- TMOMikamo14/TMOMikamo14.cpp
+++ TMOMikamo14/TMOMikamo14.cpp
@@ -29,6 +29,33 @@ TMOMikamo14::~TMOMikamo14()
  * --------------------------------------------------------------------------- */
 int TMOMikamo14::Transform()
 {
+	double *pSourceData = pSrc->GetData();
+	double *pDestinationData = pDst->GetData();
+
+	double luminanceAverage = 0;
+
+	for (int x = 0; x < pSrc->GetWidth(); x++)
+		for (int y = 0; y < pSrc->GetHeight(); y++)
+		{
+			luminanceAverage += pSrc->GetLuminance(x, y);
+		}
+
+	luminanceAverage /= pSrc->GetHeight() * pSrc->GetWidth();
+
+	double lambda_l, lambda_m, lambda_s, k1, k2, k3, k4, k5, k6;
+
+	lambda_l = -18.3 / (1 + 7.2 * pow(I, -0.7)) - 0.9;
+	lambda_m = -44.6 / (1 + 35.4 * pow(I, -1.2)) + 22.0;
+	lambda_s = 43.0 / (1 + 9.0 * pow(I, -1.5)) + 28.0;
+	k1 = 6.69 / (1 + 2500 * pow(I, -2.65)) + 0.80;
+	k2 = -6.24 / (1 + 2500 * pow(I, -2.5)) - 0.77;
+	k3 = 0.36 / (1 + 50.02 * pow(I, -1.5)) + 0.04;
+	k4 = 0.24 / (1 + 50.04 * pow(I, -1.7)) + 0.03;
+	k5 = 0.42 / (1 + 1.76 * pow(I, -0.02)) + 0.14;
+	k6 = 0.15 / (1 + 2.8 * pow(I, -0.46)) - 0.27;
+
+	
+
 	// Source image is stored in local parameter pSrc
 	// Destination image is in pDst
 
