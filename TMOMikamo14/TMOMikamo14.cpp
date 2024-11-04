0efe14ae493c7a1c4d8c3cf9f6b8969cc90a2994
findraj <jankofindra@gmail.com>
2024-11-04 16:47:44 +0000

    fix: added usage of matrixes, changed computing of LMS

--- TMOMikamo14/TMOMikamo14.cpp
+++ TMOMikamo14/TMOMikamo14.cpp
@@ -9,17 +9,17 @@
  * --------------------------------------------------------------------------- */
 TMOMikamo14::TMOMikamo14()
 {
-	SetName(L"Mikamo14");					  // TODO - Insert operator name
-	SetDescription(L"Add your TMO description here"); // TODO - Insert description
+	SetName(L"Mikamo14");
+	SetDescription(L"Add your TMO description here");
 
-	ri.SetName(L"ri");				// TODO - Insert parameters names
+	ri.SetName(L"ri");
 	ri.SetDescription(L"Retinal illuminance in Trolands (Td)\n"
-										"Photopic vision: RI >= 10\n"
-										"Mesopic vision: 10 >= RI >= 0.01\n"
-										"Scotopic vision: 0.01 >= RI"); // TODO - Insert parameter descriptions
-	ri.SetDefault(150.0);							// TODO - Add default values
+					  "Photopic vision: RI >= 10\n"
+					  "Mesopic vision: 10 >= RI >= 0.01\n"
+					  "Scotopic vision: 0.01 >= RI");
+	ri.SetDefault(150.0);
 	ri = 150.0;
-	ri.SetRange(0.0, 1000.0); // TODO - Add acceptable range if needed
+	ri.SetRange(0.0, 1000.0);
 	this->Register(ri);
 }
 
@@ -28,156 +28,168 @@ TMOMikamo14::~TMOMikamo14()
 }
 
 // Function to compute adapted luminance (I)
-double TMOMikamo14::computeAdaptedLuminance(int x, int y) {
-    // Compute the scene luminance from the RGB values of the pixel
-    double L = pSrc->GetLuminance(x, y);
-    
-    // Compute the pupil area based on the luminance
-    // Avoid very small or negative luminance values
-    if (L < 0.001) L = 0.001;
+double TMOMikamo14::computeAdaptedLuminance(int x, int y)
+{
+	// Compute the scene luminance from the RGB values of the pixel
+	double L = pSrc->GetLuminance(x, y);
 
-    // Empirical formula to compute pupil diameter
-    double pupilDiameter = (5.0 / 3.0) * std::exp(-0.1 * std::log(L));
-    
-    // Convert diameter to area (A = π * r^2)
-    double A = M_PI * std::pow(pupilDiameter / 2.0, 2);
-    
-    // Adapted luminance I = A * L
-    return A * L;
+	// Compute the pupil area based on the luminance
+	// Avoid very small or negative luminance values
+	if (L < 0.001)
+		L = 0.001;
+
+	// Empirical formula to compute pupil diameter
+	double pupilDiameter = (5.0 / 3.0) * std::exp(-0.1 * std::log(L));
+
+	// Convert diameter to area (A = π * r^2)
+	double A = M_PI * std::pow(pupilDiameter / 2.0, 2);
+
+	// Adapted luminance I = A * L
+	return A * L;
 }
 
-std::vector<double> TMOMikamo14::computeSigmoidParams(double I) {
-    // Compute parameter functions for different levels of adaptation
-    std::vector<double> params(9);
+std::vector<double> TMOMikamo14::computeSigmoidParams(double I)
+{
+	// Compute parameter functions for different levels of adaptation
+	std::vector<double> params(9);
 
-    params[0] = -18.3 / (1 + 7.2 * std::pow(I, -0.7)) - 0.9; // λl(I)
-    params[1] = -44.6 / (1 + 35.4 * std::pow(I, -1.2)) + 22.0; // λm(I)
-    params[2] = 43.0 / (1 + 9.0 * std::pow(I, -1.5)) + 28.0;  // λs(I)
+	params[0] = -18.3 / (1 + 7.2 * std::pow(I, -0.7)) - 0.9;   // λl(I)
+	params[1] = -44.6 / (1 + 35.4 * std::pow(I, -1.2)) + 22.0; // λm(I)
+	params[2] = 43.0 / (1 + 9.0 * std::pow(I, -1.5)) + 28.0;   // λs(I)
 
-    params[3] = 6.69 / (1 + 2500 * std::pow(I, -2.65)) + 0.80; // k1(I)
-    params[4] = -6.24 / (1 + 2500 * std::pow(I, -2.5)) - 0.77; // k2(I)
-    params[5] = 0.36 / (1 + 50.02 * std::pow(I, -1.5)) + 0.04; // k3(I)
+	params[3] = 6.69 / (1 + 2500 * std::pow(I, -2.65)) + 0.80; // k1(I)
+	params[4] = -6.24 / (1 + 2500 * std::pow(I, -2.5)) - 0.77; // k2(I)
+	params[5] = 0.36 / (1 + 50.02 * std::pow(I, -1.5)) + 0.04; // k3(I)
 
-    params[6] = 0.24 / (1 + 50.04 * std::pow(I, -1.7)) + 0.03; // k4(I)
-    params[7] = 0.42 / (1 + 1.76 * std::pow(I, -0.02)) + 0.14; // k5(I)
-    params[8] = 0.15 / (1 + 2.80 * std::pow(I, -0.46)) - 0.27; // k6(I)
+	params[6] = 0.24 / (1 + 50.04 * std::pow(I, -1.7)) + 0.03; // k4(I)
+	params[7] = 0.42 / (1 + 1.76 * std::pow(I, -0.02)) + 0.14; // k5(I)
+	params[8] = 0.15 / (1 + 2.80 * std::pow(I, -0.46)) - 0.27; // k6(I)
 
-    return params;
+	return params;
 }
 
-double TMOMikamo14::getIntensityFromWavelength(std::vector<double> spectrum, double lambda) {
-	// spectrum is a vector of 10 values, each representing the SPD at a specific wavelength (380nm to 720nm)
-	double position = lambda - 380.0 / 340.0; // - first wavelength / wavelength range
+int TMOMikamo14::findBinFromI(std::vector<double>spectrum, double I)
+{
+	int bin = 0;
+	double max = 0.0;
 
-	if (position < 0) {
-		return spectrum[0];
-	}
-
-	if (position >= 9) {
-		return spectrum[9];
-	}
-
-	int i = static_cast<int>(std::floor(position));
-	double fraction = position - i;
-
-	return spectrum[i] + (spectrum[i + 1] - spectrum[i]) * fraction;
-}
-
-double TMOMikamo14::getWavelengthFromIntensity(std::vector<double> spectrum, double intensity) {
-	// spectrum is a vector of 10 values, each representing the SPD at a specific wavelength (380nm to 720nm)
-	for (int i = 0; i < 9; i++) {
-		if (spectrum[i] <= intensity && spectrum[i + 1] >= intensity) {
-			double fraction = (intensity - spectrum[i]) / (spectrum[i + 1] - spectrum[i]);
-			return 380.0 + 340.0 * (i + fraction);
+	for (int i = 0; i < bins; i++)
+	{
+		if (spectrum[i] > max)
+		{
+			max = spectrum[i];
+			bin = i;
 		}
 	}
 
-	return 720.0;
+	return bin;
 }
 
-TMOMikamo14::OpponentColor TMOMikamo14::applyTwoStageModel(std::vector<double> spectrum, double I) {
-	std::vector<double> paramsOld = computeSigmoidParams(I);
-	std::vector<double> paramsNew = computeSigmoidParams(ri);
-
-	// TODO: How to calculate Cl, Cm, Cs?
-	double L_wavelength = getWavelengthFromIntensity(spectrum, I);
-	double M_wavelength = getWavelengthFromIntensity(spectrum, I);
-	double S_wavelength = getWavelengthFromIntensity(spectrum, I);
-
-
-    double L_adj = getIntensityFromWavelength(spectrum, L_wavelength - paramsOld[0]);
-    double M_adj = getIntensityFromWavelength(spectrum, M_wavelength - paramsOld[1]);
-    double S_adj = getIntensityFromWavelength(spectrum, S_wavelength - paramsOld[2]);
-    
-    OpponentColor color;
-    color.achromatic = 0.6 * L_adj + 0.4 * M_adj;
-    color.redGreen = paramsNew[3] * L_adj + paramsNew[4] * M_adj + paramsNew[5] * S_adj;
-    color.yellowBlue = paramsNew[6] * L_adj + paramsNew[7] * M_adj + paramsNew[8] * S_adj;
-
-    return color;
+int TMOMikamo14::findNewBin(int oldBin, double diff)
+{
+	if (diff > 0.0)
+	{
+		int binTop = (oldBin + 1) * binWidth;
+		int move = std::round((binTop - diff) / binWidth);
+		return oldBin - move;
+	}
+	else if (diff < 0.0)
+	{
+		int binBottom = oldBin * binWidth;
+		int move = std::round((binBottom - diff) / binWidth);
+		return oldBin + move;
+	}
+	
+	return oldBin;
 }
 
-std::vector<double> TMOMikamo14::RGBtoSpectrum(int x, int y)
+cv::Mat TMOMikamo14::applyTwoStageModel(LMS lms, double I)
+{
+	std::vector<double> params = computeSigmoidParams(I);
+
+	// find wavelength of original LMS values
+	int binL = findBinFromI(lms.L, I);
+	int binM = findBinFromI(lms.M, I);
+	int binS = findBinFromI(lms.S, I);
+	int newBinL = findNewBin(binL, params[0]);
+	int newBinM = findNewBin(binM, params[1]);
+	int newBinS = findNewBin(binS, params[2]);
+	double newL = lms.L[newBinL];
+	double newM = lms.M[newBinM];
+	double newS = lms.S[newBinS];
+
+	cv::Mat opponentColor(3, 1, CV_64F);
+	cv::Mat LMS = (cv::Mat_<double>(3, 1) << newL, newM, newS);
+	cv::Mat paramsMat = (cv::Mat_<double>(3, 3) << 0.6, 0.4, 0.0, params[3], params[4], params[5], params[6], params[7], params[8]);
+
+	opponentColor = paramsMat * LMS;
+
+	std::vector<double> newParams = computeSigmoidParams(ri);
+	cv::Mat newParamsMat = (cv::Mat_<double>(3, 3) << 0.6, 0.4, 0.0, newParams[3], newParams[4], newParams[5], newParams[6], newParams[7], newParams[8]);
+	newParamsMat = newParamsMat.inv();
+	
+	opponentColor = newParamsMat * opponentColor;
+
+	return opponentColor;
+}
+
+std::vector<double> TMOMikamo14::RGBtoSpectrum(double red, double green, double blue)
 {
 	// Return vector
 	std::vector<double> spectrum;
-	// Get the RGB values of the pixel
-	double red = pSrc->GetPixel(x, y)[0];
-	double green = pSrc->GetPixel(x, y)[1];
-	double blue = pSrc->GetPixel(x, y)[2];
 
 	for (int binIndex = 0; binIndex < bins; binIndex++)
 	{
 		// Access SPDs from color_data for the specific bin (bin_index)
-		double white_spd   = color_data[binIndex][White];
-		double cyan_spd    = color_data[binIndex][Cyan];
+		double white_spd = color_data[binIndex][White];
+		double cyan_spd = color_data[binIndex][Cyan];
 		double magenta_spd = color_data[binIndex][Magenta];
-		double yellow_spd  = color_data[binIndex][Yellow];
-		double red_spd     = color_data[binIndex][Red];
-		double green_spd   = color_data[binIndex][Green];
-		double blue_spd    = color_data[binIndex][Blue];
+		double yellow_spd = color_data[binIndex][Yellow];
+		double red_spd = color_data[binIndex][Red];
+		double green_spd = color_data[binIndex][Green];
+		double blue_spd = color_data[binIndex][Blue];
 		double spd = 0.0;
 		if (red <= green && red <= blue)
 		{
-    		spd += white_spd * red;
-    		if (green <= blue)
+			spd += white_spd * red;
+			if (green <= blue)
 			{
-        		spd += cyan_spd * (green - red);
-        		spd += blue_spd * (blue - green);
-    		}
+				spd += cyan_spd * (green - red);
+				spd += blue_spd * (blue - green);
+			}
 			else
 			{
-        		spd += cyan_spd * (blue - red);
-        		spd += green_spd * (green - blue);
-    		}	
-		} 
+				spd += cyan_spd * (blue - red);
+				spd += green_spd * (green - blue);
+			}
+		}
 		else if (green <= red && green <= blue)
 		{
-    		spd += white_spd * green;
-    		if (red <= blue)
+			spd += white_spd * green;
+			if (red <= blue)
 			{
-        		spd += magenta_spd * (red - green);
-        		spd += blue_spd * (blue - red);
-    		}
+				spd += magenta_spd * (red - green);
+				spd += blue_spd * (blue - red);
+			}
 			else
 			{
-        		spd += magenta_spd * (blue - green);
-        		spd += red_spd * (red - blue);
-    		}
-		} 		
+				spd += magenta_spd * (blue - green);
+				spd += red_spd * (red - blue);
+			}
+		}
 		else
 		{
-    		spd += white_spd * blue;
-    		if (red <= green)
+			spd += white_spd * blue;
+			if (red <= green)
 			{
-        		spd += yellow_spd * (red - blue);
-        		spd += green_spd * (green - red);
-    		}
+				spd += yellow_spd * (red - blue);
+				spd += green_spd * (green - red);
+			}
 			else
 			{
-        		spd += yellow_spd * (green - blue);
-        		spd += red_spd * (red - green);
-    		}
+				spd += yellow_spd * (green - blue);
+				spd += red_spd * (red - green);
+			}
 		}
 		spectrum.push_back(spd);
 	}
@@ -194,15 +206,18 @@ int TMOMikamo14::Transform()
 	{
 		for (int x = 0; x < pSrc->GetWidth(); x++)
 		{
-			double* pixel = pSrc->GetPixel(x, y);
+			double *pixel = pSrc->GetPixel(x, y);
 			double I = computeAdaptedLuminance(x, y);
-			std::vector<double> spectrum = RGBtoSpectrum(x, y);
-			OpponentColor oColor = applyTwoStageModel(spectrum, I);
+			LMS lms;
+			lms.L = RGBtoSpectrum(pixel[0], 0, 0);
+			lms.M = RGBtoSpectrum(0, pixel[1], 0);
+			lms.S = RGBtoSpectrum(0, 0, pixel[2]);
+			cv::Mat opponentColor = applyTwoStageModel(lms, I);
 
-			double* outputPixel = pDst->GetPixel(x, y);
-			outputPixel[0] = oColor.achromatic;
-			outputPixel[1] = oColor.redGreen;
-			outputPixel[2] = oColor.yellowBlue;
+			double *outputPixel = pDst->GetPixel(x, y);
+			outputPixel[0] = opponentColor.at<double>(0, 0);
+			outputPixel[1] = opponentColor.at<double>(1, 0);
+			outputPixel[2] = opponentColor.at<double>(2, 0);
 		}
 	}
 	pDst->Convert(TMO_RGB);
--- TMOMikamo14/TMOMikamo14.h
+++ TMOMikamo14/TMOMikamo14.h
@@ -3,40 +3,49 @@
 class TMOMikamo14 : public TMO
 {
 public:
-	struct OpponentColor
+	struct LMS
 	{
-    	double achromatic;
-    	double redGreen;
-    	double yellowBlue;
+		std::vector<double> L;
+		std::vector<double> M;
+		std::vector<double> S;
 	};
 	TMOMikamo14();
 	virtual ~TMOMikamo14();
 	virtual int Transform();
 	double computeAdaptedLuminance(int x, int y);
 	std::vector<double> computeSigmoidParams(double I);
-	double getIntensityFromWavelength(std::vector<double> spectrum, double lambda);
-	double getWavelengthFromIntensity(std::vector<double> spectrum, double intensity);
-	OpponentColor applyTwoStageModel(std::vector<double> spectrum, double I);
-	std::vector<double> RGBtoSpectrum(int x, int y);
-
+	int findBinFromI(std::vector<double> spectrum, double I);
+	int findNewBin(int oldBin, double diff);
+	cv::Mat applyTwoStageModel(LMS lms, double I);
+	std::vector<double> RGBtoSpectrum(double red, double green, double blue);
 
 	// 10 bin spectral representation from 380nm to 720nm. The bins are all equal size. source: https://www.researchgate.net/profile/Shahab-Askarian/post/What_is_a_good_way_to_convert_a_RGB_pixel_to_a_wavelength/attachment/59d62ba6c49f478072e9d9a7/AS%3A273535619010561%401442227382042/download/An+RGB+to+Spectrum+Conversion+for+Reflectances.pdf
 	const static int bins = 10;
 	const static int colors = 7;
-	enum color { White, Cyan, Magenta, Yellow, Red, Green, Blue };
+	const static int binWidth = 34;
+	enum color
+	{
+		White,
+		Cyan,
+		Magenta,
+		Yellow,
+		Red,
+		Green,
+		Blue
+	};
 
 	double color_data[bins][colors] = {
-        {1.0000, 0.9710, 1.0000, 0.0001, 0.1012, 0.0000, 1.0000},  // Bin 1
-        {1.0000, 0.9426, 1.0000, 0.0000, 0.0515, 0.0000, 1.0000},  // Bin 2
-        {0.9999, 1.0007, 0.9685, 0.1088, 0.0000, 0.0273, 0.8916},  // Bin 3
-        {0.9993, 1.0007, 0.2229, 0.6651, 0.0000, 0.7937, 0.3323},  // Bin 4
-        {0.9992, 1.0007, 0.0000, 1.0000, 0.0000, 1.0000, 0.0000},  // Bin 5
-        {0.9998, 1.0007, 0.0458, 1.0000, 0.0000, 0.9418, 0.0000},  // Bin 6
-        {1.0000, 0.1564, 0.8369, 0.9996, 0.8325, 0.1719, 0.0003},  // Bin 7
-        {1.0000, 0.0000, 1.0000, 0.9586, 1.0149, 0.0000, 0.0369},  // Bin 8
-        {1.0000, 0.0000, 1.0000, 0.9685, 1.0149, 0.0000, 0.0483},  // Bin 9
-        {1.0000, 0.0000, 0.9959, 0.9840, 1.0149, 0.0025, 0.0496}   // Bin 10
-    };
+		{1.0000, 0.9710, 1.0000, 0.0001, 0.1012, 0.0000, 1.0000}, // Bin 1
+		{1.0000, 0.9426, 1.0000, 0.0000, 0.0515, 0.0000, 1.0000}, // Bin 2
+		{0.9999, 1.0007, 0.9685, 0.1088, 0.0000, 0.0273, 0.8916}, // Bin 3
+		{0.9993, 1.0007, 0.2229, 0.6651, 0.0000, 0.7937, 0.3323}, // Bin 4
+		{0.9992, 1.0007, 0.0000, 1.0000, 0.0000, 1.0000, 0.0000}, // Bin 5
+		{0.9998, 1.0007, 0.0458, 1.0000, 0.0000, 0.9418, 0.0000}, // Bin 6
+		{1.0000, 0.1564, 0.8369, 0.9996, 0.8325, 0.1719, 0.0003}, // Bin 7
+		{1.0000, 0.0000, 1.0000, 0.9586, 1.0149, 0.0000, 0.0369}, // Bin 8
+		{1.0000, 0.0000, 1.0000, 0.9685, 1.0149, 0.0000, 0.0483}, // Bin 9
+		{1.0000, 0.0000, 0.9959, 0.9840, 1.0149, 0.0025, 0.0496}  // Bin 10
+	};
 
 protected:
 	TMODouble ri;
