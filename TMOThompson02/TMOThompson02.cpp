8e1070835e9a2104802a54e9428ad7e9b4d869b1
findraj <jankofindra@gmail.com>
2025-02-16 10:43:20 +0100

    feat: luminance compression

--- TMOThompson02/TMOThompson02.cpp
+++ TMOThompson02/TMOThompson02.cpp
@@ -24,47 +24,74 @@ TMOThompson02::~TMOThompson02()
 {
 }
 
-/* --------------------------------------------------------------------------- *
- * This overloaded function is an implementation of your tone mapping operator *
- * --------------------------------------------------------------------------- */
-int TMOThompson02::Transform()
+TMOThompson02::matAndDouble TMOThompson02::getLuminanceMat()
 {
-	// Source image is stored in local parameter pSrc
-	// Destination image is in pDst
+	matAndDouble result;
+	double max = 0.0;
+	cv::Mat luminanceMat(pSrc->GetWidth(), pSrc->GetHeight(), CV_64F);
 
-	// Initialy images are in RGB format, but you can
-	// convert it into other format
-	pSrc->Convert(TMO_Yxy); // This is format of Y as luminance
-	pDst->Convert(TMO_Yxy); // x, y as color information
-
-	double *pSourceData = pSrc->GetData();		// You can work at low level data
-	double *pDestinationData = pDst->GetData(); // Data are stored in form of array
-												// of three doubles representing
-												// three colour components
-	double pY, px, py;
-
-	int j = 0;
-	for (j = 0; j < pSrc->GetHeight(); j++)
+	for (int y = 0; y < pSrc->GetHeight(); y++)
 	{
-		pSrc->ProgressBar(j, pSrc->GetHeight()); // You can provide progress bar
-		for (int i = 0; i < pSrc->GetWidth(); i++)
+		for (int x = 0; x < pSrc->GetWidth(); x++)
 		{
-			pY = *pSourceData++;
-			px = *pSourceData++;
-			py = *pSourceData++;
-
-			// Here you can use your transform
-			// expressions and techniques...
-			pY *= dParameter; // Parameters can be used like
-							  // simple variables
-
-			// and store results to the destination image
-			*pDestinationData++ = pY;
-			*pDestinationData++ = px;
-			*pDestinationData++ = py;
+			double *pixel = pSrc->GetPixel(x, y);
+			double L = 0.27 * pixel[0] + 0.67 * pixel[1] + 0.06 * pixel[2];
+			luminanceMat.at<double>(x, y) = L;
+			if (L > max)
+			{
+				max = L;
+			}
 		}
 	}
-	pSrc->ProgressBar(j, pSrc->GetHeight());
+
+	result.mat = luminanceMat;
+	result.d = max;
+	return result;
+}
+
+double TMOThompson02::getDisplayableLuminance(double L, double maxLuminance)
+{
+	return (L * (1 + L / (maxLuminance * maxLuminance))) / (1 + L);
+}
+
+void TMOThompson02::mapLuminance(cv::Mat &luminanceMat, double maxLuminance)
+{
+	for (int y = 0; y < pSrc->GetHeight(); y++)
+	{
+		for (int x = 0; x < pSrc->GetWidth(); x++)
+		{
+			double L = luminanceMat.at<double>(x, y);
+			double Ld = getDisplayableLuminance(L, maxLuminance);
+			luminanceMat.at<double>(x, y) = Ld;
+		}
+	}
+}
+
+int TMOThompson02::Transform()
+{
+	TMOThompson02::matAndDouble result = getLuminanceMat();
+	cv::Mat luminanceMat = result.mat;
+	double maxLuminance = result.d;
+
+	mapLuminance(luminanceMat, maxLuminance);
+
+	pSrc->Convert(TMO_Yxy);
+	pDst->Convert(TMO_Yxy);
+
+	for (int y = 0; y < pDst->GetHeight(); y++)
+	{
+		for (int x = 0; x < pDst->GetWidth(); x++)
+		{
+			double *srcPixel = pSrc->GetPixel(x, y);
+			double *dstPixel = pDst->GetPixel(x, y);
+
+			dstPixel[0] = luminanceMat.at<double>(x, y);
+			dstPixel[1] = srcPixel[1];
+			dstPixel[2] = srcPixel[2];
+		}
+	}
+
 	pDst->Convert(TMO_RGB);
+
 	return 0;
 }
--- TMOThompson02/TMOThompson02.h
+++ TMOThompson02/TMOThompson02.h
@@ -3,8 +3,17 @@
 class TMOThompson02 : public TMO
 {
 public:
+	typedef struct matAndDouble
+	{
+		cv::Mat mat;
+		double d;
+	} matAndDouble;
+
 	TMOThompson02();
 	virtual ~TMOThompson02();
+	matAndDouble getLuminanceMat();
+	double getDisplayableLuminance(double L, double maxLuminance);
+	void mapLuminance(cv::Mat &luminanceMat, double maxLuminance);
 	virtual int Transform();
 
 protected:
